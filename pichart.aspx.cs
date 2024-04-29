7e0b4fe01df5233c73a784086bd3bfad91b84b52
ali candar <118173006+coolwli@users.noreply.github.com>
2024-04-29 19:19:57 +0300

    Update pichart.aspx.cs

--- pichart.aspx.cs
+++ pichart.aspx.cs
@@ -12,42 +12,56 @@ namespace fizkselArayuz
 {
     public partial class pichart : System.Web.UI.Page
     {
+        Dictionary<string, Dictionary<string, int>> columnUniqueValues;
         protected void Page_Load(object sender, EventArgs e)
         {
-            string scriptCode = "var veri =JSON.parse(localStorage.getItem('dizi'))";
-            ClientScript.RegisterStartupScript(this.GetType(), "LocalDataScript", scriptCode, true);
-            string veri = Page.ClientScript.GetWebResourceUrl(this.GetType(), "veri");
-            Response.Write(HiddenField1.Value+" dc " +veri);
+
+            if (IsPostBack)
+            {
+                Response.Write("");
+            }
+            else
+                Response.Write(Request["hdnTestControl"]);
+
         }
 
-
-
-        private void WriteChartToDiv(Dictionary<string, int> dict)
+        private void WriteChartToDiv()
         {
-            Chart chart = new Chart();
-            chart.Width = 400;
-            chart.Height = 300;
-
-            ChartArea chartArea = new ChartArea();
-            chart.ChartAreas.Add(chartArea);
-
-            Series series = new Series();
-            series.ChartType = SeriesChartType.Pie;
-
-            foreach (var kvp in dict)
+            foreach (var column in columnUniqueValues)
             {
-                DataPoint point = new DataPoint();
-                point.SetValueY(kvp.Value);
-                point.Label = $"{kvp.Key} ({kvp.Value})"; // Include count in the label
-                series.Points.Add(point);
+                HtmlGenericControl divChart = new HtmlGenericControl("div");
+                divChart.Attributes["class"] = "chart-container";
+
+                Chart chart = new Chart();
+                chart.Width = 400;
+                chart.Height = 400;
+                chart.ChartAreas.Add(new ChartArea());
+                chart.Titles.Add(column.Key + " Grafiği");
+
+                Series series = new Series();
+                series.ChartType = SeriesChartType.Pie;
+                series.IsValueShownAsLabel = false;
+
+                foreach (var valueCount in column.Value)
+                {
+                    DataPoint point = new DataPoint();
+
+                    point.SetValueY(valueCount.Value);
+                    point.LegendText = $"{valueCount.Key} ({valueCount.Value})";
+                    series.Points.Add(point);
+                }
+
+                chart.Series.Add(series);
+
+                Legend legend = new Legend();
+                legend.Docking = Docking.Right;
+                chart.Legends.Add(legend);
+
+                divChart.Controls.Add(chart);
+
+                chartsContainer.Controls.Add(divChart);
             }
-
-            chart.Series.Add(series);
-
-            Legend legend = new Legend();
-            legend.Docking = Docking.Bottom;
-            chart.Legends.Add(legend);
-
+            /*            
             using (MemoryStream ms = new MemoryStream())
             {
                 chart.SaveImage(ms, ChartImageFormat.Png);
@@ -57,7 +71,34 @@ namespace fizkselArayuz
                 
                 chartContainer.Controls.Add(nevDiv);
             }
+            */
         }
 
+        protected void btnFoo_Click(object sender, EventArgs e)
+        {
+            string hiddenControlValue = Request["hdnTestControl"];
+            string[] vs = hiddenControlValue.Split(new[] { "\r\n", "\n", "\r" }, StringSplitOptions.None);
+            columnUniqueValues = new Dictionary<string, Dictionary<string, int>>();
+
+            foreach (string value in vs)
+            {
+                if (value.Length==0) continue;
+
+                string columName = value.Split('~')[0];
+                string[] values = value.Split('~')[1].Split('%');
+
+                columnUniqueValues.Add(columName, new Dictionary<string, int>());
+                foreach(string opp in values)
+                {
+                    if (opp.Length == 0) continue;
+                    string key = opp.Split('!')[0];
+                    int count = Convert.ToInt32(opp.Split('!')[1]);
+                    columnUniqueValues[columName].Add(key, count);
+                }
+
+            }
+
+            WriteChartToDiv();
+        }
     }
 }
