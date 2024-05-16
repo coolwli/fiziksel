235347c30495cd4d7ba262b82644af481e97cd9a
ali candar <118173006+coolwli@users.noreply.github.com>
2024-05-16 22:03:33 +0300

    Update default.aspx.cs

--- default.aspx.cs
+++ default.aspx.cs
@@ -1,160 +1,72 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
+using System.Data;
 using System.Linq;
-using System.Web.UI;
-using System.Web.UI.DataVisualization.Charting;
 using System.Web.UI.HtmlControls;
-using OfficeOpenXml;
-using OfficeOpenXml.Drawing;
-using OfficeOpenXml.Drawing.Chart;
+using System.Data.SqlClient;
 
-namespace WebApplication1
+
+namespace vminfo
 {
-    public partial class FileName : System.Web.UI.Page
+    public partial class _default : System.Web.UI.Page
     {
-        Dictionary<string, Dictionary<string, int>> columnUniqueValuesCount;
-
+        SqlConnection con = new SqlConnection(@"Data Source=TEKSCR1\SQLEXPRESS;Initial Catalog=CloudUnited;Integrated Security=True");
         protected void Page_Load(object sender, EventArgs e)
         {
-            columnUniqueValuesCount = new Dictionary<string, Dictionary<string, int>>();
-
-            for (int rowIndex = 1; rowIndex < dataTable.Rows.Count; rowIndex++)
+            if (con.State == ConnectionState.Open)
             {
-                HtmlTableRow row = dataTable.Rows[rowIndex];
-                for (int i = 0; i < row.Cells.Count; i++)
-                {
-                    string columnName = dataTable.Rows[0].Cells[i].InnerText;
-                    string cellValue = row.Cells[i].InnerText;
-
-                    if (!columnUniqueValuesCount.ContainsKey(columnName))
-                    {
-                        columnUniqueValuesCount[columnName] = new Dictionary<string, int>();
-                    }
-
-                    if (!columnUniqueValuesCount[columnName].ContainsKey(cellValue))
-                    {
-                        columnUniqueValuesCount[columnName][cellValue] = 1;
-                    }
-                    else
-                    {
-                        columnUniqueValuesCount[columnName][cellValue]++;
-                    }
-                }
+                con.Close();
             }
-            foreach (var column in columnUniqueValuesCount)
-            {
-                HtmlGenericControl divChart = new HtmlGenericControl("div");
-                divChart.Attributes["class"] = "chart-container";
-
-                Chart chart = new Chart();
-                chart.Width = 400;
-                chart.Height = 400;
-                chart.ChartAreas.Add(new ChartArea());
-                chart.Titles.Add(column.Key + " Grafiği");
-
-                Series series = new Series();
-                series.ChartType = SeriesChartType.Pie;
-                series.IsValueShownAsLabel = false;
-
-                foreach (var valueCount in column.Value)
-                {
-                    DataPoint point = new DataPoint();
-
-                    point.SetValueY(valueCount.Value);
-                    point.LegendText = $"{valueCount.Key} ({valueCount.Value})";
-
-                    if (ShouldShowLabel((double)valueCount.Value / column.Value.Values.Sum()))
-                    {
-                        point.Label = valueCount.Key;
-                        point.IsValueShownAsLabel = true;
-                    }
-                    else
-                    {
-                        point.IsValueShownAsLabel = false;
-                    }
-
-                    series.Points.Add(point);
-                }
-
-                chart.Series.Add(series);
-
-                Legend legend = new Legend();
-                legend.Docking = Docking.Right;
-                chart.Legends.Add(legend);
-
-                divChart.Controls.Add(chart);
-
-                chartsContainer.Controls.Add(divChart);
-            }
-
+            con.Open();
+            showList();
         }
-
-        private bool ShouldShowLabel(double value)
-        {
-            double threshold = 0.5; 
-            return value >= threshold;
-        }
-
-        protected void btnCreateCharts_Click(object sender, EventArgs e)
+        public void showList()
         {
 
-            CreateExcelFile();
-        }
-        private void CreateExcelFile()
-        {
-            ExcelPackage.LicenseContext = LicenseContext.NonCommercial; 
+            string vc = vcDropdown.SelectedValue;
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "UPDATE VMInfos2 SET VMOwner = NULL WHERE VMOwner ='' ";
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = $"SELECT VMName,VMNumCPU,VMMemoryCapacity,VMTotalDisk,VMPowerState,VMCluster,VMDataCenter,VMOwner,VMCreatedDate FROM VMInfos2 WHERE vCenter = '{vc}' ORDER BY CASE WHEN VMOwner IS NULL THEN 1 ELSE 0 END, VMName ASC";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            cmd.CommandText = "SELECT CreatedDate,VMName FROM VMsExtras";
+            cmd.ExecuteNonQuery();
+            DataTable dtt = new DataTable();
+            SqlDataAdapter da2 = new SqlDataAdapter(cmd);
+            da2.Fill(dtt);
 
-            using (ExcelPackage excelPackage = new ExcelPackage())
+
+            if (tableBody != null)
             {
-                ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.Add("Charts");
-
-                int startRow = 1;
-
-                foreach (var column in columnUniqueValuesCount)
+                int count = 1;
+                foreach (DataRow row in dt.Rows)
                 {
-                    int startColumn = 1;
-                    var pieChart = worksheet.Drawings.AddChart("PieChart_" + column.Key, eChartType.Pie);
-                    pieChart.Border.LineStyle = eLineStyle.Solid;
-                    pieChart.SetPosition(startRow - 1, 0, startColumn + 2, 0);
-                    pieChart.SetSize(400, 400);
-                    pieChart.Title.Text=column.Key;
-
-
-                    worksheet.Cells[startRow, startColumn].Value = column.Key;
-                    startRow++;
-                    int dataStartRow = startRow ;
-                    foreach (var valueCount in column.Value)
+                    HtmlTableRow newRow = new HtmlTableRow();
+                    for (int j = 0; j < 9; j++)
                     {
-                        worksheet.Cells[startRow, startColumn].Value = valueCount.Key;
-                        worksheet.Cells[startRow, startColumn + 1].Value = valueCount.Value;
-                        startRow++;
+                        HtmlTableCell newCell = new HtmlTableCell();
+                        newCell.InnerText = row[j].ToString();
+                        if (j == 8)
+                        {
+                            var foundRow = dtt.AsEnumerable().ToList().FindIndex(frow => frow[1].ToString() == row[0].ToString());
+                            if (foundRow != -1)
+                            {
+                                newCell.InnerText = dtt.Rows[foundRow][0].ToString();
+                            }
+                        }
+                        newRow.Cells.Add(newCell);
                     }
+                    tableBody.Controls.Add(newRow);
+                    count++;
+                    if (count > 100) newRow.Style["display"] = "none";
 
-                    var chartDataRange = worksheet.Cells[dataStartRow, startColumn + 1, dataStartRow+column.Value.Count-1, startColumn + 1];
-                    var chartLabelsRange = worksheet.Cells[dataStartRow, startColumn, dataStartRow +column.Value.Count-1, startColumn];
-
-                    // Add chart series
-                    pieChart.Series.Add(chartDataRange, chartLabelsRange);
-
-                    startRow = startRow + 20;
                 }
 
-                // Save the Excel package
-                string filePath = Server.MapPath("~/Charts.xlsx");
-                excelPackage.SaveAs(new FileInfo(filePath));
-
-                // Send the file to the browser
-                Response.Clear();
-                Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-                Response.AppendHeader("Content-Disposition", "attachment; filename=Charts.xlsx");
-                Response.TransmitFile(filePath);
-                Response.End();
             }
         }
-
-
     }
 
 }
-
