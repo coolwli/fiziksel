a3a26b0197c65969bfb5fd231dea1dba60b00ff6
ali candar <118173006+coolwli@users.noreply.github.com>
2024-05-20 10:24:11 +0300

    Update default.aspx.cs

--- default.aspx.cs
+++ default.aspx.cs
@@ -1,72 +1,91 @@
 using System;
 using System.Data;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web.UI.HtmlControls;
 using System.Data.SqlClient;
-
 
 namespace vminfo
 {
     public partial class _default : System.Web.UI.Page
     {
-        SqlConnection con = new SqlConnection(@"Data Source=TEKSCR1\SQLEXPRESS;Initial Catalog=CloudUnited;Integrated Security=True");
-        protected void Page_Load(object sender, EventArgs e)
+        private readonly string connectionString = @"Data Source=TEKSCR1\SQLEXPRESS;Initial Catalog=CloudUnited;Integrated Security=True";
+
+        protected async void Page_Load(object sender, EventArgs e)
         {
-            if (con.State == ConnectionState.Open)
+            if (!IsPostBack)
             {
-                con.Close();
+                await ShowListAsync();
             }
-            con.Open();
-            showList();
         }
-        public void showList()
+
+        public async Task ShowListAsync()
         {
+            DataTable vmInfos = new DataTable();
+            DataTable vmExtras = new DataTable();
 
-            SqlCommand cmd = con.CreateCommand();
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "UPDATE VMInfos2 SET VMOwner = NULL WHERE VMOwner ='' ";
-            cmd.ExecuteNonQuery();
-            cmd.CommandText = $"SELECT VMName,vCenter,VMNumCPU,VMMemoryCapacity,VMTotalDisk,VMPowerState,VMCluster,VMDataCenter,VMOwner,VMCreatedDate FROM VMInfos2 ORDER BY CASE WHEN VMOwner IS NULL THEN 1 ELSE 0 END, VMName ASC";
-            cmd.ExecuteNonQuery();
-            DataTable dt = new DataTable();
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-            da.Fill(dt);
-            cmd.CommandText = "SELECT CreatedDate,VMName FROM VMsExtras";
-            cmd.ExecuteNonQuery();
-            DataTable dtt = new DataTable();
-            SqlDataAdapter da2 = new SqlDataAdapter(cmd);
-            da2.Fill(dtt);
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                await con.OpenAsync();
 
+                using (SqlCommand cmd = con.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.Text;
+
+                    // Update VMOwner to NULL if it's empty
+                    cmd.CommandText = "UPDATE VMInfos2 SET VMOwner = NULL WHERE VMOwner ='' ";
+                    await cmd.ExecuteNonQueryAsync();
+
+                    // Select VMInfos2 data
+                    cmd.CommandText = "SELECT VMName,vCenter,VMNumCPU,VMMemoryCapacity,VMTotalDisk,VMPowerState,VMCluster,VMDataCenter,VMOwner,VMCreatedDate FROM VMInfos2 ORDER BY CASE WHEN VMOwner IS NULL THEN 1 ELSE 0 END, VMName ASC";
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        da.Fill(vmInfos);
+                    }
+
+                    // Select VMsExtras data
+                    cmd.CommandText = "SELECT CreatedDate,VMName FROM VMsExtras";
+                    using (SqlDataAdapter da2 = new SqlDataAdapter(cmd))
+                    {
+                        da2.Fill(vmExtras);
+                    }
+                }
+            }
 
             if (tableBody != null)
             {
                 int count = 1;
-                foreach (DataRow row in dt.Rows)
+                foreach (DataRow row in vmInfos.Rows)
                 {
                     HtmlTableRow newRow = new HtmlTableRow();
                     for (int j = 0; j < 10; j++)
                     {
-                        HtmlTableCell newCell = new HtmlTableCell();
-                        newCell.InnerText = row[j].ToString();
+                        HtmlTableCell newCell = new HtmlTableCell
+                        {
+                            InnerText = row[j].ToString()
+                        };
+
                         if (j == 9)
                         {
-                            var foundRow = dtt.AsEnumerable().ToList().FindIndex(frow => frow[1].ToString() == row[0].ToString());
-                            if (foundRow != -1)
+                            var foundRow = vmExtras.AsEnumerable().FirstOrDefault(frow => frow.Field<string>("VMName") == row.Field<string>("VMName"));
+                            if (foundRow != null)
                             {
-                                newCell.InnerText = dtt.Rows[foundRow][0].ToString();
+                                newCell.InnerText = foundRow.Field<DateTime>("CreatedDate").ToString();
                             }
                         }
+
                         newRow.Cells.Add(newCell);
                         newRow.Style["display"] = "none";
                     }
+
                     tableBody.Controls.Add(newRow);
                     count++;
-                    if (count > 100) newRow.Attributes["class"] = "in-filter";
-
+                    if (count > 100)
+                    {
+                        newRow.Attributes["class"] = "in-filter";
+                    }
                 }
-
             }
         }
     }
-
 }
