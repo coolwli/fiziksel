cc0a390495d7b4c4e85762c2a03e5d6d4d38af59
ali candar <118173006+coolwli@users.noreply.github.com>
2024-05-23 21:11:31 +0300

    Update pichart.aspx.cs

--- pichart.aspx.cs
+++ pichart.aspx.cs
@@ -1,70 +1,23 @@
-using System;
-using System.Data;
-using System.Data.SqlClient;
-using System.Web.UI;
+function sortDatas(index) {
+    filteredData.sort(function (a, b) {
+        let aValue = a[Object.keys(a)[index]];
+        let bValue = b[Object.keys(b)[index]];
 
-namespace vminfo
-{
-    public partial class _default : System.Web.UI.Page
-    {
-        private readonly string connectionString = @"Data Source=TEKSCR1\SQLEXPRESS;Initial Catalog=CloudUnited;Integrated Security=True";
+        // Check if values are dates (in MM/DD/YYYY format)
+        const datePattern = /^\d{2}\/\d{2}\/\d{4}$/;
 
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            if (!IsPostBack)
-            {
-                ShowList();
-            }
+        if (datePattern.test(aValue) && datePattern.test(bValue)) {
+            aValue = new Date(aValue);
+            bValue = new Date(bValue);
+            return ascending ? aValue - bValue : bValue - aValue;
         }
 
-        public void ShowList()
-        {
-            DataTable vmInfos = new DataTable();
-            DataTable vmExtras = new DataTable();
-
-
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                con.Open();
-
-                using (SqlCommand cmd = con.CreateCommand())
-                {
-                    cmd.CommandType = CommandType.Text;
-
-                    cmd.CommandText = "SELECT VMName,vCenter,VMNumCPU,VMMemoryCapacity,VMTotalDisk,VMPowerState,VMCluster,VMDataCenter,VMOwner,VMCreatedDate FROM VMInfos2";
-                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
-                    {
-                        da.Fill(vmInfos);
-                    }
-
-                    cmd.CommandText = "SELECT CreatedDate,VMName FROM VMsExtras";
-                    using (SqlDataAdapter da2 = new SqlDataAdapter(cmd))
-                    {
-                        da2.Fill(vmExtras);
-                    }
-                }
-            }
-
-            string jsArray = "data = [";
-            foreach (DataRow row in vmInfos.Rows)
-            {
-                jsArray += "{";
-                jsArray += $"Name: '{row["VMName"]}',";
-                jsArray += $"vCenter: '{row["vCenter"]}',";
-                jsArray += $"CPU: '{row["VMNumCPU"]}',";
-                jsArray += $"Memory: '{row["VMMemoryCapacity"]}',";
-                jsArray += $"TotalDisk: '{row["VMTotalDisk"]}',";
-                jsArray += $"PowerState: '{row["VMPowerState"]}',";
-                jsArray += $"Cluster: '{row["VMCluster"]}',";
-                jsArray += $"DataCenter: '{row["VMDataCenter"]}',";
-                jsArray += $"Owner: '{row["VMOwner"]}',";
-                jsArray += $"CreatedDate: '{row["VMCreatedDate"]}',";
-                jsArray += "},";
-            }
-            jsArray = jsArray.TrimEnd(',');
-            jsArray += "]; initializeTable();";
-
-            ClientScript.RegisterStartupScript(this.GetType(), "initializeData", jsArray, true);
+        if (!isNaN(aValue) && !isNaN(bValue)) {
+            return ascending ? aValue - bValue : bValue - aValue;
         }
-    }
+
+        return ascending ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
+    });
+    currentPage = 1;
+    renderPage(currentPage);
 }
