47050745714a58166ed0a792933cd20b0540f334
ali candar <118173006+coolwli@users.noreply.github.com>
2024-05-24 23:12:40 +0300

    Update _.js

--- _.js
+++ _.js
@@ -1,1 +1,27 @@
+function sortDatas(index, ascending) {
+    filteredData.sort(function (a, b) {
+        let aValue = a[Object.keys(a)[index]];
+        let bValue = b[Object.keys(b)[index]];
 
+        // Check if values are dates (in MM/DD/YYYY format)
+        const datePattern = /^\d{2}\/\d{2}\/\d{4}$/;
+
+        if (datePattern.test(aValue) && datePattern.test(bValue)) {
+            aValue = new Date(aValue);
+            bValue = new Date(bValue);
+            return ascending ? aValue - bValue : bValue - aValue;
+        }
+
+        // Check if values are numbers
+        if (!isNaN(aValue) && !isNaN(bValue)) {
+            aValue = parseFloat(aValue);
+            bValue = parseFloat(bValue);
+            return ascending ? aValue - bValue : bValue - aValue;
+        }
+
+        // Default to string comparison
+        return ascending ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
+    });
+    currentPage = 1;
+    renderPage(currentPage);
+}
