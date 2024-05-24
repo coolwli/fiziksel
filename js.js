dffda0b1293dc1cc3af0ed5777fd93fc4adef8c0
ali candar <118173006+coolwli@users.noreply.github.com>
2024-05-24 11:34:55 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,1 @@
-    const values = [...new Set(filteredData.map(row => row[Object.keys(row)[columnIndex]]))];
+const values = [...new Set(filteredData.map(row => row[Object.keys(row)[columnIndex]].trim()))];
