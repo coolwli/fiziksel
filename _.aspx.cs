3c14f2897211c6ea385855dcf7926b2170a84e69
ali candar <118173006+coolwli@users.noreply.github.com>
2024-06-24 09:27:29 +0300

    Update _.aspx.cs

--- _.aspx.cs
+++ _.aspx.cs
@@ -6,39 +6,52 @@ namespace CloudUnited
 {
     public partial class _default : System.Web.UI.Page
     {
-
         protected void Page_Load(object sender, EventArgs e)
         {
-
-            string membersPath = @"F:/Ali/CloudUnited/Datas/Members.xlsx";
-            string directionsPath = @"F:/Ali/CloudUnited/Datas/Directions.xlsx";
-            string servicesPath = @"F:/Ali/CloudUnited/Datas/Services.xlsx";
-
-            loadMembers(membersPath);
-            loadServices(servicesPath);
-            loadDirections(directionsPath);
-
+            string excelPath = @"F:/Ali/CloudUnited/Datas/CombinedData.xlsx";
+            
+            // Tek bir bağlantı kur
+            Excel.Application excelApp = new Excel.Application();
+            Excel.Workbook wb = excelApp.Workbooks.Open(excelPath);
+            
+            // Her bir worksheet'i ayrı ayrı yükle
+            Excel.Worksheet wsMembers = wb.Worksheets["Members"];
+            Excel.Worksheet wsDirections = wb.Worksheets["Directions"];
+            Excel.Worksheet wsServices = wb.Worksheets["Services"];
+            
+            // Verileri yükle
+            loadMembers(wsMembers);
+            loadServices(wsServices);
+            loadDirections(wsDirections);
+            
+            // Workbook'u kapat ve Excel uygulamasını kapat
+            wb.Close(false);
+            excelApp.Quit();
+            
+            // Kaynakları serbest bırak
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(wsMembers);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(wsDirections);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(wsServices);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(wb);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
         }
 
-        void loadMembers(string path)
+        void loadMembers(Excel.Worksheet ws)
         {
-            Excel.Application excelApp = new Excel.Application();
-            Excel.Workbook wb = excelApp.Workbooks.Open(path);
-            Excel.Worksheet ws = wb.Worksheets[1];
             string cities = "cities=[";
             string members = "members=[";
             Excel.Range range = ws.UsedRange;
 
-            for (int col = 1; col <= range.Rows[1].Cells.Count; col++)
+            for (int col = 1; col <= range.Columns.Count; col++)
             {
-                string colText = range.Rows[1].Cells[col].Text;
+                string colText = range.Cells[1, col].Text;
                 cities += "'" + colText + "'";
                 cities += ",";
                 members += "[";
                 for (int i = 2; i <= range.Rows.Count; i++)
                 {
-                    if (range.Rows[i].Cells[col].Text == "") break;
-                    members += "'" + range.Rows[i].Cells[col].Text + "'";
+                    if (range.Cells[i, col].Text == "") break;
+                    members += "'" + range.Cells[i, col].Text + "'";
                     members += ",";
                 }
                 members = members.Substring(0, members.Length - 1);
@@ -52,75 +65,42 @@ namespace CloudUnited
 
             string script = $"<script>{members}; {cities}; showAll();</script>";
             ClientScript.RegisterStartupScript(this.GetType(), "CallMyFunction", script);
-
-            wb.Close(false);
-            excelApp.Quit();
-
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(ws);
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(wb);
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
         }
 
-        void loadDirections(string path)
+        void loadDirections(Excel.Worksheet ws)
         {
-            Excel.Application excelApp = new Excel.Application();
-            Excel.Workbook wb = excelApp.Workbooks.Open(path);
-            Excel.Worksheet ws = wb.Worksheets[1];
             Excel.Range range = ws.UsedRange;
-            for (int i =2; i <= range.Rows.Count; i++)
+            for (int i = 2; i <= range.Rows.Count; i++)
             {
                 HtmlGenericControl div = new HtmlGenericControl("div");
                 div.Attributes.Add("class", "direction-panel");
                 HtmlGenericControl h3 = new HtmlGenericControl("h3");
-                h3.ID = range.Rows[i].Cells[3].Text;
-                h3.InnerText= range.Rows[i].Cells[1].Text;
+                h3.ID = range.Cells[i, 3].Text;
+                h3.InnerText = range.Cells[i, 1].Text;
                 HtmlGenericControl p = new HtmlGenericControl("p");
-                p.InnerText = range.Rows[i].Cells[2].Text;
+                p.InnerText = range.Cells[i, 2].Text;
                 div.Controls.Add(h3);
                 div.Controls.Add(p);
                 directions.Controls.Add(div);
             }
-
-            wb.Close(false);
-            excelApp.Quit();
-
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(ws);
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(wb);
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
-
-
         }
 
-        void loadServices(string path)
+        void loadServices(Excel.Worksheet ws)
         {
-            Excel.Application excelApp = new Excel.Application();
-            Excel.Workbook wb = excelApp.Workbooks.Open(path);
-            Excel.Worksheet ws = wb.Worksheets[1];
             Excel.Range range = ws.UsedRange;
             for (int i = 2; i <= range.Rows.Count; i++)
             {
                 HtmlGenericControl div = new HtmlGenericControl("div");
                 div.Attributes.Add("class", "service-panel");
                 HtmlGenericControl h3 = new HtmlGenericControl("h3");
-                h3.ID = range.Rows[i].Cells[3].Text;
-                h3.InnerText = range.Rows[i].Cells[1].Text;
+                h3.ID = range.Cells[i, 3].Text;
+                h3.InnerText = range.Cells[i, 1].Text;
                 HtmlGenericControl p = new HtmlGenericControl("p");
-                p.InnerText = range.Rows[i].Cells[2].Text;
+                p.InnerText = range.Cells[i, 2].Text;
                 div.Controls.Add(h3);
                 div.Controls.Add(p);
                 services.Controls.Add(div);
             }
-
-            wb.Close(false);
-            excelApp.Quit();
-
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(ws);
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(wb);
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
-
         }
-
-
     }
-
 }
