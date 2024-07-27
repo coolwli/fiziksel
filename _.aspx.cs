8e9ec26704edeb5315315e2dc3f6499bc7fc4623
ali candar <118173006+coolwli@users.noreply.github.com>
2024-07-27 18:03:22 +0300

    Update _.aspx.cs

--- _.aspx.cs
+++ _.aspx.cs
@@ -1,53 +1,113 @@
 using System;
-using System.Net.Http;
 using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Linq;
+using System.Web.UI;
+using System.Xml.Linq;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 
-namespace YourNamespace
+namespace blank_page
 {
-    public partial class Default : System.Web.UI.Page
-    {
-        private const string VROPS_URL = "https://vrops.example.com/suite-api/api/auth/token";
-        private const string USERNAME = "your_username";
-        private const string PASSWORD = "your_password";
 
+    public partial class _default : System.Web.UI.Page
+    {
+        string token = "";
         protected void Page_Load(object sender, EventArgs e)
         {
+            ServicePointManager.ServerCertificateValidationCallback += new RemoteCertificateValidationCallback(validateServerCertificate);
+        }
+        private static bool validateServerCertificate(Object sender,X509Certificate certificate,X509Chain chain,SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
         }
 
-        protected async void GetTokenButton_Click(object sender, EventArgs e)
+        protected async void Button1_Click(object sender, EventArgs e)
         {
-            try
+            if (token == "")
             {
-                string token = await GetTokenAsync();
-                TokenLabel.Text = "Token: " + token;
-            }
-            catch (Exception ex)
-            {
-                TokenLabel.Text = "Error: " + ex.Message;
-            }
-        }
+                string apiUrl = "https://ptekvrops01.fw.garanti.com.tr/suite-api/api/auth/token/acquire?_no_links=true";
+                string requestBody = "{ \"username\": \"admin\", \"password\": \"VMware123!\" }"; 
 
-        private async Task<string> GetTokenAsync()
-        {
-            using (var client = new HttpClient())
-            {
-                var requestContent = new FormUrlEncodedContent(new[]
+                try
                 {
-                    new KeyValuePair<string, string>("username", USERNAME),
-                    new KeyValuePair<string, string>("password", PASSWORD),
-                    new KeyValuePair<string, string>("grant_type", "password")
-                });
+                    string tokenXml = await getApiToken(apiUrl, requestBody);
+                    Button1_Click(sender, e);
+                    XDocument xdoc = XDocument.Parse(tokenXml);
 
-                var response = await client.PostAsync(VROPS_URL, requestContent);
+                    XNamespace opsNamespace = "http://webservice.vmware.com/vRealizeOpsMgr/1.0/";
 
-                if (response.IsSuccessStatusCode)
-                {
-                    var responseContent = await response.Content.ReadAsStringAsync();
-                    return responseContent; // JSON response
+                    XElement tokenElement = xdoc.Descendants(opsNamespace + "token").FirstOrDefault();
+
+                    if (tokenElement != null)
+                    {
+                        token = tokenElement.Value;
+                        Label.InnerText="Token: " + token; 
+                    }
+                    else
+                    {
+                        Label.InnerText="Token bulunamadı.";
+                    }
+
                 }
-                else
+                catch (Exception ex)
                 {
-                    throw new HttpRequestException($"Token retrieval failed. Status Code: {response.StatusCode}");
+                    Label.InnerText = "Error1: " + ex.Message;
+                }
+            }
+            else 
+            {
+                string apiUrl = "https://ptekvrops01.fw.garanti.com.tr/suite-api/internal/views/97a71f3c-98c2-44fb-8b2b-233925936368/data/export?resourceId=98141705-743b-4083-87cf-4f8f6cedcaa3&traversalSpec=vSphere Hosts and Clusters-VMWARE-vSphere World&_ack=true";
+
+                try
+                {
+                    string result = await getApiResponse(apiUrl, token);
+                    Label.InnerText =result;
+                }
+                catch (Exception ex)
+                {
+                    Label.InnerText = "Error2: " + ex.Message;
+
+                }
+            }
+            
+        }
+
+        private async Task<string> getApiResponse(string url, string token)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.CreateHttp(url);
+            request.Method = "GET";
+            request.Headers["Authorization"] = "Bearer " + token;
+
+            using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
+            {
+                using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                {
+                    return await reader.ReadToEndAsync();
+                }
+            }
+        }
+        private async Task<string> getApiToken(string url, string requestBody)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(requestBody);
+            request.ContentLength = byteArray.Length;
+
+            using (Stream dataStream = await request.GetRequestStreamAsync())
+            {
+                dataStream.Write(byteArray, 0, byteArray.Length);
+            }
+
+            using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
+            {
+                using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                {
+                    return await reader.ReadToEndAsync();
                 }
             }
         }
