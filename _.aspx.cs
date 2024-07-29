67673918b468f0affe830883c7828d939af2a5e2
ali candar <118173006+coolwli@users.noreply.github.com>
2024-07-29 17:08:38 +0300

    Update _.aspx.cs

--- _.aspx.cs
+++ _.aspx.cs
@@ -1,25 +1,14 @@
-private async Task<string> GetVmId(string vmName)
-{
-    string getIdUrl = $"{VropsServer}/suite-api/api/resources?resourceKind=VirtualMachine&name={vmName}";
+ XmlDocument xmlDoc = new XmlDocument();
+    xmlDoc.LoadXml(xmlData);
 
-    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(getIdUrl);
-    request.Method = "GET";
-    request.Headers["Authorization"] = $"vRealizeOpsToken {token}";
+    // Identifier değerini alıyoruz
+    XmlNamespaceManager nsManager = new XmlNamespaceManager(xmlDoc.NameTable);
+    nsManager.AddNamespace("ops", "http://webservice.vmware.com/vRealizeOpsMgr/1.0/");
 
-    using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
+    XmlNode identifierNode = xmlDoc.SelectSingleNode("//ops:resource/@identifier", nsManager);
+    if (identifierNode != null)
     {
-        using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
-        {
-            string responseText = await reader.ReadToEndAsync();
-            XDocument xdoc = XDocument.Parse(responseText);
-            XNamespace opsNamespace = "http://webservice.vmware.com/vRealizeOpsMgr/1.0/";
-            XElement resourceElement = xdoc.Descendants(opsNamespace + "resource").FirstOrDefault();
-            if (resourceElement != null)
-            {
-                string identifier = resourceElement.Attribute("identifier")?.Value;
-                return identifier;
-            }
-            return null;
-        }
+        string identifier = identifierNode.Value;
+        // identifier değeriyle yapılacak işlemler
+        Response.Write("Identifier: " + identifier);
     }
-}
