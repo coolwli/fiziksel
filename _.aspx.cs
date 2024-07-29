25178f7116677deaa1cdc875a08789f8276c9cce
ali candar <118173006+coolwli@users.noreply.github.com>
2024-07-29 15:04:30 +0300

    Update _.aspx.cs

--- _.aspx.cs
+++ _.aspx.cs
@@ -1,83 +1,95 @@
 using System;
-using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
-using System.Net;
-using System.Text;
 using System.Linq;
-using System.Net.WebSockets;
-using System.Web.UI;
-using System.Xml.Linq;
+using System.Net;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
-using System.Web.Script.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI;
+using System.Xml.Linq;
 
 namespace blank_page
 {
-
     public partial class _default : System.Web.UI.Page
     {
-        string vropsServer = "https://ptekvrops01.fw.garanti.com.tr";
-        string token ="";
+        private const string VropsServer = "https://ptekvrops01.fw.garanti.com.tr";
+        private const string OpsNamespace = "http://webservice.vmware.com/vRealizeOpsMgr/1.0/";
+        private string token = "";
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            ServicePointManager.ServerCertificateValidationCallback += new RemoteCertificateValidationCallback(validateServerCertificate);
+            ServicePointManager.ServerCertificateValidationCallback += new RemoteCertificateValidationCallback(ValidateServerCertificate);
         }
-        private static bool validateServerCertificate(Object sender,X509Certificate certificate,X509Chain chain,SslPolicyErrors sslPolicyErrors)
+
+        private static bool ValidateServerCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
         {
             return true;
         }
 
         protected async void Button1_Click(object sender, EventArgs e)
         {
-            if (token=="")
+            if (string.IsNullOrEmpty(token))
             {
-                string apiUrl = "https://ptekvrops01.fw.garanti.com.tr/suite-api/api/auth/token/acquire?_no_links=true";
-                string requestBody = "{ \"username\": \"admin\", \"password\": \"VMware123!\" }"; 
-
-                try
-                {
-                    string tokenXml = await getApiToken(apiUrl, requestBody);
-                    XDocument xdoc = XDocument.Parse(tokenXml);
-
-                    XNamespace opsNamespace = "http://webservice.vmware.com/vRealizeOpsMgr/1.0/";
-
-                    XElement tokenElement = xdoc.Descendants(opsNamespace + "token").FirstOrDefault();
-
-                    if (tokenElement != null)
-                    {
-                        token = tokenElement.Value;
-                        Label.InnerText="Token: " + token; 
-                    }
-                    else
-                    {
-                        Label.InnerText="Token bulunamadı.";
-                    }
-                    Button1_Click(sender, e);
-
-
-                }
-                catch (Exception ex)
-                {
-                    Label.InnerText = "Error1: " + ex.Message;
-                }
+                await AcquireToken();
             }
-            else 
+            else
             {
-                try
-                {
-                    string result = await getVMID("tekscr1", token);
-                    label2.InnerText = result;
-                }
-                catch (Exception ex)
-                {
-                    label2.InnerText = "Error2: " + ex.Message;
-
-                }
+                await FetchVmUsageData();
             }
-            
         }
 
-        private async Task<string> getApiToken(string url, string requestBody)
+        private async Task AcquireToken()
+        {
+            string apiUrl = $"{VropsServer}/suite-api/api/auth/token/acquire?_no_links=true";
+            string requestBody = "{ \"username\": \"admin\", \"password\": \"VMware123!\" }";
+
+            try
+            {
+                string tokenXml = await GetApiToken(apiUrl, requestBody);
+                XDocument xdoc = XDocument.Parse(tokenXml);
+                XElement tokenElement = xdoc.Descendants(XName.Get("token", OpsNamespace)).FirstOrDefault();
+
+                if (tokenElement != null)
+                {
+                    token = tokenElement.Value;
+                    Label.InnerText = $"Token: {token}";
+                }
+                else
+                {
+                    Label.InnerText = "Token bulunamadı.";
+                }
+            }
+            catch (Exception ex)
+            {
+                Label.InnerText = $"Error acquiring token: {ex.Message}";
+            }
+        }
+
+        private async Task FetchVmUsageData()
+        {
+            try
+            {
+                string vmId = await GetVmId("tekscr1");
+                if (!string.IsNullOrEmpty(vmId))
+                {
+                    string usageData = await GetUsageData(vmId);
+                    label2.InnerText = usageData;
+                }
+                else
+                {
+                    label2.InnerText = "VM ID not found.";
+                }
+            }
+            catch (Exception ex)
+            {
+                label2.InnerText = $"Error fetching VM data: {ex.Message}";
+            }
+        }
+
+        private async Task<string> GetApiToken(string url, string requestBody)
         {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.Method = "POST";
@@ -100,47 +112,70 @@ namespace blank_page
             }
         }
 
-        private async Task<string> getVMID(string vmName, string token)
+        private async Task<string> GetVmId(string vmName)
         {
-            
-            string getId = $"https://ptekvrops01.fw.garanti.com.tr/suite-api/api/resources?resourceKind?=VirtualMachine&name=tekscr1";
+            string getIdUrl = $"{VropsServer}/suite-api/api/resources?resourceKind=VirtualMachine&name={vmName}";
 
-
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(getId);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(getIdUrl);
             request.Method = "GET";
-            request.Headers["Authorization"] = "vRealizeOpsToken " + token;
+            request.Headers["Authorization"] = $"vRealizeOpsToken {token}";
+
             using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
             {
                 using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
                 {
-                    return await reader.ReadToEndAsync();
+                    string responseText = await reader.ReadToEndAsync();
+                    XDocument xdoc = XDocument.Parse(responseText);
+                    XElement resourceElement = xdoc.Descendants(XName.Get("resource", OpsNamespace)).FirstOrDefault();
+                    return resourceElement?.Element(XName.Get("identifier", OpsNamespace))?.Value;
                 }
             }
         }
 
-        private async Task<string> getUsageDatas(string vmId, string token)
+        private async Task<string> GetUsageData(string vmId)
         {
             DateTime startTime = DateTime.UtcNow.AddDays(-30);
             DateTime endTime = DateTime.UtcNow;
 
-            DateTimeOffset startOffset = new DateTimeOffset(startTime);
-            long startTimeMillis = startOffset.ToUnixTimeMilliseconds();
+            long startTimeMillis = new DateTimeOffset(startTime).ToUnixTimeMilliseconds();
+            long endTimeMillis = new DateTimeOffset(endTime).ToUnixTimeMilliseconds();
 
-            DateTimeOffset endOffset = new DateTimeOffset(endTime);
-            long endTimeMillis = endOffset.ToUnixTimeMilliseconds();
-
-            string metricsUrl = $"{vropsServer}/suite-api/api/resources/{vmId}/stats?statKey=cpu|usage_average&begin={startTimeMillis}&end={endTimeMillis}&intervalQuantifier=3&intervalType=HOURS&rollUpType=AVG";
+            string metricsUrl = $"{VropsServer}/suite-api/api/resources/{vmId}/stats?statKey=cpu|usage_average&begin={startTimeMillis}&end={endTimeMillis}&intervalQuantifier=3&intervalType=HOURS&rollUpType=AVG";
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(metricsUrl);
             request.Method = "GET";
-            request.Headers["Authorization"] = "vRealizeOpsToken " + token;
+            request.Headers["Authorization"] = $"vRealizeOpsToken {token}";
+
             using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
             {
                 using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
                 {
-                    return await reader.ReadToEndAsync();
+                    string responseXml = await reader.ReadToEndAsync();
+                    return ParseUsageData(responseXml);
                 }
             }
         }
 
+        private string ParseUsageData(string xmlData)
+        {
+            XDocument xdoc = XDocument.Parse(xmlData);
+            XElement stats = xdoc.Descendants(XName.Get("stat", OpsNamespace)).FirstOrDefault();
+
+            if (stats == null)
+            {
+                return "No stats data found.";
+            }
+
+            long[] timestamps = stats.Element(XName.Get("timestamps", OpsNamespace)).Value.Split(' ').Select(long.Parse).ToArray();
+            double[] data = stats.Element(XName.Get("data", OpsNamespace)).Value.Split(' ').Select(double.Parse).ToArray();
+
+            var result = new List<string>();
+            for (int i = 0; i < timestamps.Length; i++)
+            {
+                DateTime dateTime = DateTimeOffset.FromUnixTimeMilliseconds(timestamps[i]).UtcDateTime;
+                result.Add($"{dateTime:yyyy-MM-dd HH:mm:ss}: {data[i]}%");
+            }
+
+            return string.Join("<br/>", result);
+        }
     }
 }
