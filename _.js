2c1d6349e4a7a704053fb26df4389f0b725b98d1
ali candar <118173006+coolwli@users.noreply.github.com>
2024-07-30 09:00:06 +0300

    Update _.js

--- _.js
+++ _.js
@@ -1,6 +1,6 @@
-
-            JavaScriptSerializer serializer = new JavaScriptSerializer();
-            serializer.MaxJsonLength = Int32.MaxValue;
-            string json = serializer.Serialize(rows);
-            string script = $"<script> data = {json}; initializeTable(); screenName = 'vmscreen'; </script>";
-            ClientScript.RegisterStartupScript(this.GetType(), "initializeData", script);
+<configuration>
+  <appSettings>
+    <add key="VropsUsername" value="admin" />
+    <add key="VropsPassword" value="your_secure_password" />
+  </appSettings>
+</configuration>
