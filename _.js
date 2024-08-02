6e8922881ab9ab9e12cd8b35cc93f397166835b2
ali candar <118173006+coolwli@users.noreply.github.com>
2024-08-02 16:09:58 +0300

    Update _.js

--- _.js
+++ _.js
@@ -1,257 +1,201 @@
-        <script>
-            const urlParams = new URLSearchParams(window.location.search);
-            const id = urlParams.get("id");
+<script>
+    // URL Parametresinden ID'yi Alma
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get("id");
+    document.getElementById("baslik").textContent = id;
 
-            const baslik = document.getElementById("baslik");
-            baslik.textContent = id;
-       
-            function sortTable(columnIndex, column) {
-                var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-                table = column.closest('table');
+    // Tablo Sıralama Fonksiyonu
+    function sortTable(columnIndex, column) {
+        const table = column.closest('table');
+        const rows = Array.from(table.rows).slice(1);
+        let switching = true;
+        let dir = "asc";
+
+        while (switching) {
+            switching = false;
+            for (let i = 0; i < rows.length - 1; i++) {
+                let shouldSwitch = false;
+                let xValue = rows[i].getElementsByTagName("td")[columnIndex].innerText;
+                let yValue = rows[i + 1].getElementsByTagName("td")[columnIndex].innerText;
+
+                if (!isNaN(xValue) && !isNaN(yValue)) {
+                    xValue = parseFloat(xValue);
+                    yValue = parseFloat(yValue);
+                }
+                if ((dir === "asc" && xValue > yValue) || (dir === "desc" && xValue < yValue)) {
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+            if (shouldSwitch) {
+                [rows[i], rows[i + 1]] = [rows[i + 1], rows[i]];
                 switching = true;
-                dir = "asc";
-                rows = Array.from(table.rows).slice(1);
-    
-                while (switching) {
-                    switching = false;
-
-                    for (i = 0; i < rows.length - 1; i++) {
-                        shouldSwitch = false;
-                        xValue = rows[i].getElementsByTagName("td")[columnIndex].innerText;
-                        yValue = rows[i + 1].getElementsByTagName("td")[columnIndex].innerText;
-            
-                        if (!isNaN(xValue) && !isNaN(yValue)) {
-                            xValue = parseFloat(xValue);
-                            yValue = parseFloat(yValue);
-                        }
-                        if (dir == "asc") {
-                            if (xValue > yValue) {
-                                shouldSwitch = true;
-                                break;
-                            }
-                        } else if (dir == "desc") {
-                            if (xValue < yValue) {
-                                shouldSwitch = true;
-                                break;
-                            }
-                        }
-                    }
-                    if (shouldSwitch) {
-                        [rows[i], rows[i + 1]] = [rows[i + 1], rows[i]];
-                        switching = true;
-                        switchcount++;
-                    } else {
-                        if (switchcount == 0 && dir == "asc") {
-                            dir = "desc";
-                            switching = true;
-                        }
-                    }
-                }
-
-                for (i = 0; i < rows.length; i++) {
-                    table.appendChild(rows[i]);
-                }
+            } else if (dir === "asc") {
+                dir = "desc";
+                switching = true;
             }
+        }
+        rows.forEach(row => table.appendChild(row));
+    }
 
-            function openTab(panelId, button,performanceButton) {
-                event.preventDefault();
-                var i, tabcontent, tabbuttons;
-                tabcontent = document.getElementsByClassName("columnRight");
-                tabbuttons = document.getElementsByClassName("tab-button");
+    // Sekme Açma Fonksiyonu
+    function openTab(event, panelId, button) {
+        event.preventDefault();
+        const tabContent = document.getElementsByClassName("columnRight");
+        const tabButtons = document.getElementsByClassName("tab-button");
 
-                for (i = 0; i < tabcontent.length; i++) {
-                    tabcontent[i].style.display = "none";
-                }
-                for (i = 0; i < tabbuttons.length; i++) {
-                    tabbuttons[i].classList.remove("active");
-                }
+        Array.from(tabContent).forEach(content => content.style.display = "none");
+        Array.from(tabButtons).forEach(btn => btn.classList.remove("active"));
 
-                document.getElementById(panelId).style.display = "flex";
-                button.classList.add("active");
-                
-            }
-            
+        document.getElementById(panelId).style.display = "flex";
+        button.classList.add("active");
+    }
 
-            document.getElementById("host").addEventListener("click", function () {
-                window.location.href = "hostscreen.aspx?id=" + document.getElementById("host").textContent;
-            });
-            document.getElementById("cluster").addEventListener("click", function () {
-                window.location.href = "clusterscreen.aspx?id=" + document.getElementById("cluster").textContent;
-            });
-        </script>
-        <script>
-            const createChart = (ctx, data, dates, label, borderColor) => {
-                const min = Math.min(...data.map(d => d.y));
-                const max = Math.max(...data.map(d => d.y));
+    // Host ve Cluster Navigasyonu
+    document.getElementById("host").addEventListener("click", function () {
+        window.location.href = `hostscreen.aspx?id=${this.textContent}`;
+    });
+    document.getElementById("cluster").addEventListener("click", function () {
+        window.location.href = `clusterscreen.aspx?id=${this.textContent}`;
+    });
+</script>
 
-                const minIndex = data.findIndex(d => d.y === min);
-                const maxIndex = data.findIndex(d => d.y === max);
 
-                const config = {
-                    type: 'line',
-                    data: {
-                        labels: dates,
-                        datasets: [
-                            {
-                                label: 'Minimum: %' + min,
-                                data: [{ x: dates[minIndex], y: min }],
-                                borderColor: 'red',
-                                fill: true,
-                                pointRadius: 6,
-                                pointHoverRadius: 4,
-                                pointBackgroundColor: 'red'
-                            },
-                            {
-                                label: 'Maximum: %' + max,
-                                data: [{ x: dates[maxIndex], y: max }],
-                                borderColor: 'black',
-                                fill: true,
-                                pointRadius: 6,
-                                pointHoverRadius: 4,
-                                pointBackgroundColor: 'black'
-                            },
-                            {
-                                label: label,
-                                data: data,
-                                borderColor: borderColor,
-                                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                                fill: true,
-                                pointRadius: 0,
-                                pointHoverRadius: 4,
-                                borderWidth: 1,
-                                tension: 0.01
-                            }
-                        ]
+<script>
+    const createChart = (ctx, data, dates, label, borderColor) => {
+        const min = Math.min(...data.map(d => d.y));
+        const max = Math.max(...data.map(d => d.y));
+        const minIndex = data.findIndex(d => d.y === min);
+        const maxIndex = data.findIndex(d => d.y === max);
+
+        const config = {
+            type: 'line',
+            data: {
+                labels: dates,
+                datasets: [
+                    {
+                        label: `Minimum: ${min}`,
+                        data: [{ x: dates[minIndex], y: min }],
+                        borderColor: 'red',
+                        fill: true,
+                        pointRadius: 6,
+                        pointHoverRadius: 4,
+                        pointBackgroundColor: 'red'
                     },
-                    options: {
-                        responsive: true,
-                        plugins: {
-                            title: {
-                                display: true,
-                                text: `Chart.js Line Chart - ${label}`
-                            }
-                        },
-                        interaction: {
-                            intersect: false
-                        },
-                        scales: {
-                            x: {
-                                type: 'time',
-                                time: {
-                                    unit: 'day'
-                                },
-                                display: true,
-                                title: {
-                                    display: true,
-                                    text: 'Date'
-                                }
-                            },
-                            y: {
-                                display: true,
-                                title: {
-                                    display: true,
-                                    text: 'Value'
-                                },
-                                suggestedMin: Math.max(0, min - 10),
-                                suggestedMax: max + 10
-                            }
-                        }
+                    {
+                        label: `Maximum: ${max}`,
+                        data: [{ x: dates[maxIndex], y: max }],
+                        borderColor: 'black',
+                        fill: true,
+                        pointRadius: 6,
+                        pointHoverRadius: 4,
+                        pointBackgroundColor: 'black'
+                    },
+                    {
+                        label: label,
+                        data: data,
+                        borderColor: borderColor,
+                        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                        fill: true,
+                        pointRadius: 0,
+                        pointHoverRadius: 4,
+                        borderWidth: 1,
+                        tension: 0.01
                     }
-                };
-
-                return new Chart(ctx, config);
-            };
-
-            const processLargeData = (data, dates) => {
-                if (data.length <= 350) {
-                    return {
-                        data: data.map((value, index) => ({ x: dates[index], y: value })),
-                        dates: dates
-                    };
-                }
-
-                const reducedData = [];
-                const reducedDates = [];
-                const step = Math.ceil(data.length / 350);
-                let sum = 0;
-                let count = 0;
-                let sumDates = 0;
-                var date;
-
-                for (let i = 0; i < data.length; i++) {
-                    sum += data[i];
-                    date = new Date(dates[i])
-                    sumDates += date.getTime(); 
-                    count++;
-
-                    if ((i + 1) % step === 0) {
-                        const average = sum / count;
-                        const averageDate = new Date(sumDates / count); 
-                        reducedData.push({ x: averageDate, y: average });
-                        reducedDates.push(averageDate);
-                        sum = 0;
-                        sumDates = 0;
-                        count = 0;
+                ]
+            },
+            options: {
+                responsive: true,
+                plugins: {
+                    title: {
+                        display: true,
+                        text: `Chart.js Line Chart - ${label}`
+                    }
+                },
+                interaction: {
+                    intersect: false
+                },
+                scales: {
+                    x: {
+                        type: 'time',
+                        time: { unit: 'day' },
+                        display: true,
+                        title: { display: true, text: 'Date' }
+                    },
+                    y: {
+                        display: true,
+                        title: { display: true, text: 'Value' },
+                        suggestedMin: Math.max(0, min - 10),
+                        suggestedMax: max + 10
                     }
                 }
+            }
+        };
 
-                if (count > 0) {
-                    const average = sum / count;
-                    const averageDate = new Date(sumDates / count);
-                    reducedData.push({ x: averageDate, y: average });
-                    reducedDates.push(averageDate);
-                }
+        return new Chart(ctx, config);
+    };
 
-                const min = Math.min(...data);
-                const max = Math.max(...data);
-                const minIndex = data.indexOf(min);
-                const maxIndex = data.indexOf(max);
-                const minDate = new Date(dates[minIndex]);
-                const maxDate = new Date(dates[maxIndex]);
-                const isMinInReducedData = reducedData.some(d => d.x.getTime() === minDate.getTime());
-                const isMaxInReducedData = reducedData.some(d => d.x.getTime() === maxDate.getTime());
+    const processLargeData = (data, dates) => {
+        const maxDataPoints = 350;
+        if (data.length <= maxDataPoints) {
+            return { data: data.map((value, index) => ({ x: dates[index], y: value })), dates: dates };
+        }
 
-                if (!isMinInReducedData) {
-                    reducedData.push({ x: minDate, y: min });
-                    reducedDates.push(minDate);
-                }
-                if (!isMaxInReducedData) {
-                    reducedData.push({ x: maxDate, y: max });
-                    reducedDates.push(maxDate);
-                }
+        const step = Math.ceil(data.length / maxDataPoints);
+        const reducedData = [];
+        const reducedDates = [];
 
-                reducedData.sort((a, b) => a.x - b.x);
-                reducedDates.sort((a, b) => a - b);
+        for (let i = 0; i < data.length; i += step) {
+            const chunk = data.slice(i, i + step);
+            const avgValue = chunk.reduce((acc, curr) => acc + curr, 0) / chunk.length;
+            const avgDate = new Date(dates.slice(i, i + step).reduce((acc, curr) => acc + new Date(curr).getTime(), 0) / chunk.length);
 
-                return { dates: reducedDates, data: reducedData };
-            };
+            reducedData.push({ x: avgDate, y: avgValue });
+            reducedDates.push(avgDate);
+        }
+
+        // Min ve max değerleri indirgenmiş verilere ekleme
+        const minValues = [];
+        const maxValues = [];
+        for (let i = 0; i < data.length; i++) {
+            if (data[i] === Math.min(...data)) {
+                minValues.push({ x: new Date(dates[i]), y: data[i] });
+            }
+            if (data[i] === Math.max(...data)) {
+                maxValues.push({ x: new Date(dates[i]), y: data[i] });
+            }
+        }
+
+        // Min ve max değerleri indirgenmiş verilere ekleyelim ve tekrar sıraya koyalım
+        reducedData.push(...minValues, ...maxValues);
+        reducedData.sort((a, b) => a.x - b.x);
+        reducedDates.sort((a, b) => a - b);
+
+        return { data: reducedData, dates: reducedDates };
+    };
+
+    const fetchData = () => {
+        const timeRange = parseInt(document.getElementById('timeRange').value);
+        const filteredDates = dates.filter(dateString => new Date() - new Date(dateString) <= (timeRange * 24 * 60 * 60 * 1000));
+        const filteredCpuData = cpuDatas.slice(-filteredDates.length);
+        const filteredMemoryData = memoryDatas.slice(-filteredDates.length);
+        const processedCPUData = processLargeData(filteredCpuData, filteredDates);
+        const processedMemoryData = processLargeData(filteredMemoryData, filteredDates);
+
+        if (cpuChart) cpuChart.destroy();
+        if (memoryChart) memoryChart.destroy();
+
+        cpuChart = createChart(cpuCtx, processedCPUData.data, processedCPUData.dates, 'CPU Usage', 'rgba(75, 192, 192, 1)');
+        memoryChart = createChart(memoryCtx, processedMemoryData.data, processedMemoryData.dates, 'Memory Usage', 'rgba(153, 102, 255, 1)');
+    };
+
+    const cpuCtx = document.getElementById('cpuChart').getContext('2d');
+    const memoryCtx = document.getElementById('memoryChart').getContext('2d');
+    let cpuChart, memoryChart;
+</script>
 
 
-            const fetchData = () => {
-                const timeRange = parseInt(document.getElementById('timeRange').value);
-                const filteredDates = dates.filter(dateString => {
-                    return new Date() -new Date(dateString) <=(timeRange * 24 *60*60*1000)
-                });
-                const filteredCpuData = cpuDatas.slice(-filteredDates.length);
-                console.log(dates.length, filteredDates.length, filteredCpuData.length);
-                const filteredMemoryData = memoryDatas.slice(-filteredDates.length);
-                const processedCPUData = processLargeData(filteredCpuData, filteredDates);
-                const processedMemoryData = processLargeData(filteredMemoryData, filteredDates);
-                console.log(new Date(filteredDates[filteredDates.length - 1]).getDay()-1);
 
-                if (cpuChart) cpuChart.destroy();
-                if (memoryChart) memoryChart.destroy();
-
-                cpuChart = createChart(cpuCtx, processedCPUData.data, processedCPUData.dates, 'CPU Usage', 'rgba(75, 192, 192, 1)');
-                memoryChart = createChart(memoryCtx, processedMemoryData.data, processedMemoryData.dates, 'Memory Usage', 'rgba(153, 102, 255, 1)');
-            };
-
-            const cpuCtx = document.getElementById('cpuChart').getContext('2d');
-            const memoryCtx = document.getElementById('memoryChart').getContext('2d');
-
-            let cpuChart, memoryChart;
-
-        </script>
 
 
 
