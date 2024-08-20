b96faf7209c4a90cd24a982524808dfa03d6d1b8
ali candar <118173006+coolwli@users.noreply.github.com>
2024-08-20 09:21:08 +0300

    Update _.aspx.cs

--- _.aspx.cs
+++ _.aspx.cs
@@ -1,313 +1,140 @@
-<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="vmscreen.aspx.cs" Inherits="vminfo.vmscreen" Async="true" %>
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Web.Configuration;
+using System.Web.Script.Serialization;
 
-<html>
-<head runat="server">
-    <title>VM Screen</title>
-    <link rel="stylesheet" type="text/css" href="https://cloudunited/Styles/default-style.css" />
-    <link rel="stylesheet" type="text/css" href="screenStyle.css" />
-    <script src="chart.js"></script>
-    <script src="chartjs-adapter-date-fns.js"></script>
-    <style>
-        .panel-group {
-            gap: 5px;
-            display: flex;
-            font-size:14px;
-            flex-direction: row;
-            overflow: auto;
-            margin-top: 5px;
+namespace vminfo
+{
+    public class TokenManager
+    {
+        private const string TokenFileName = "tokens.txt"; 
+        private static readonly TimeSpan TokenLifetime = TimeSpan.FromHours(3);
+
+        private readonly HttpClient _httpClient;
+        private readonly string _username;
+        private readonly string _password;
+
+        public TokenManager(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+            _username = WebConfigurationManager.AppSettings["VropsUsername"]
+                        ?? throw new ArgumentNullException(nameof(_username));
+            _password = WebConfigurationManager.AppSettings["VropsPassword"]
+                        ?? throw new ArgumentNullException(nameof(_password));
         }
-        .disk-panel{
-            min-width: max-content;
-            padding: 10px;
-            border-radius: 8px;
-            background-color: #fff;
-            border: 1px solid #ddd;
-            cursor:pointer;
+
+        public async Task<string> GetTokenAsync(string vcenter)
+        {
+            if (vcenter == "apgaraavcs801" || vcenter == "apgartksvcs801" || vcenter == "ptekvcsd01")
+            {
+                return await CheckTokenAsync("https://apgaravrops801.fw.garanti.com.tr", "ankara"); ;
+            }
+            return await CheckTokenAsync("https://ptekvrops01.fw.garanti.com.tr", "pendik");
         }
 
-        .disk-panel.active {
-            background-color: #cccccc;
-            font-weight: bold;
-        }
+        private async Task<string> CheckTokenAsync(string vropsServer, string tokenType)
+        {
+            string tokenFilePath = GetFilePath();
+            var tokenInfo = ReadTokenInfoFromFile(tokenFilePath, tokenType);
 
-    </style>    
-</head>
-<body>
-    <form id="form1" runat="server">
-        <div class="header">
-            <h1 class="baslik" id="baslik" runat="server">Host Informations</h1>
-        </div>
-        <div class="container">
-            <div class="columnLeft">
-                <div class="panel" style="height:auto; padding-bottom: 20px; ">
-                    <div class="tabs">
-                        <button class="back-button" onclick="event.preventDefault(); window.location.href='default.aspx'">Go Back</button>
-                        <button class="tab-button active" onclick="openTab('dataColumn',this)">Data</button>
-                        <button class="tab-button" onclick="openTab('performanceColumn',this)">Performance & Disk</button>
-                    </div>
+            if (DateTime.Now >= tokenInfo.ExpiryDate)
+            {
+                string newToken = await AcquireTokenAsync(vropsServer);
 
-                    <table>
-                        <tr>
-                            <td class="bold-text">Power:</td>
-                            <td id="power" runat="server"></td>
-                        </tr>
-                        <tr>
-                            <td><br /></td>
-                        </tr>
-                        <tr>
-                            <td class="bold-text">Host:</td>
-                            <td id="host" class="link-text" runat="server"></td>
-                        </tr>
-                        <tr>
-                            <td class="bold-text">Cluster:</td>
-                            <td id="cluster" class="link-text" runat="server"></td>
-                        </tr>
-
-                        <tr>
-                            <td class="bold-text">Created Date:</td>
-                            <td id="createdDate" runat="server"></td>
-                        </tr>
-                        <tr>
-                            <td class="bold-text">OS:</td>
-                            <td id="os" runat="server"></td>
-                        </tr>
-
-                        <tr>
-                            <td class="bold-text">Data Center:</td>
-                            <td id="datacenter" runat="server"></td>
-                        </tr>
-                        <tr>
-                            <td class="bold-text">vCenter:</td>
-                            <td id="vcenter" runat="server"></td>
-                        </tr>
-                        <tr>
-                            <td class="bold-text">Host Model:</td>
-                            <td id="hostmodel" runat="server"></td>
-                        </tr>
-                        <tr>
-                            <td class="bold-text">CPU:</td>
-                            <td id="numcpu" runat="server"></td>
-                        </tr>
-                    
-                        <tr>
-                            <td class="bold-text">Memory:</td>
-                            <td id="totalmemory" runat="server"></td>
-                        </tr>
-                    </table>
-                    <div style="width:96%;margin:auto; margin-top:70px;">
-                        <div class="disk-info">
-                            <p class="disk-used" id="usedDisk" runat="server">Used Disk: <strong>300 GB</strong></p>
-                            <p class="disk-free" id="freeDisk" runat="server">Free Disk: <strong>200 GB</strong></p>
-                        </div>
-                        <div class="bar">
-                            <span style="max-width: 100%" id="usedPercentage" runat="server"></span>
-                            <div class="bar-label" id="usedBar" runat="server">60%</div>
-                        </div>
-                        <p style="font-size: 12px; color: #6c757d; text-align: center;" id="totalDisk" runat="server">Total Disk: <strong>500 GB</strong></p>
-                    </div>         
-                    <table style="margin-top:30px;">
-                       <tr>
-                            <td class="bold-text">VM Notes:</td>
-                            <td id="notes" runat="server"></td>
-                        </tr>
-                        <tr>
-                            <td><br /></td>
-                        </tr>
-                        <tr>
-                            <td class="bold-text">Last Updated Date:</td>
-                            <td id="lasttime" runat="server"></td>
-                        </tr>
-                    </table>
-
-                </div>
-            </div>
-            <div class="columnRight" id="dataColumn">
-                <div class="panel">
-                    <p class="blue-text">Network Cards</p>
-                    <table id="networkTable" class="detailsTable" runat="server">
-                        <thead>
-                            <tr>
-                            <th style="width:auto;" onclick="sortTable(0,this)">Name</th>
-                            <th style="width:auto;" onclick="sortTable(1,this)">Network</th>
-                            <th style="width:auto;" onclick="sortTable(2,this)">MAC</th>
-                            <th style="width:auto;" onclick="sortTable(2,this)">Type</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                            
-                        </tbody>
-                    </table>
-                </div>
-
-                <div class="panel">
-                    <p class="blue-text">disks</p>
-                    <table id="disksTable" class="detailsTable" runat="server">
-                        <thead>
-                            <tr>
-                            <th style="width:auto;" onclick="sortTable(0,this)">Name</th>
-                            <th style="width:auto;" onclick="sortTable(1,this)">Capacity</th>
-                            <th style="width:auto;" onclick="sortTable(2,this)">Storage Format</th>
-                            <th style="width:auto;" onclick="sortTable(3,this)">Datastore</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                            
-                        </tbody>
-                    </table>
-                </div>
-                <div class="panel" style="height: 360px;">
-                <p class="blue-text">last event logs</p>
-                <table id="eventsTable" class="detailsTable" runat="server">
-                    <thead>
-                    <tr>
-                        <th style="width:auto;" onclick="sortTable(0,this)">Event Date</th>
-                        <th style="width:auto;" onclick="sortTable(1,this)">Event</th>  
-
-                    </tr>
-                    </thead>
-                    <tbody>
-                               
-                    </tbody>
-                </table>
-            
-            </div>
-            </div>
-            <div class="columnRight" id="performanceColumn" style="display:none; padding:3px;">
-                <div class="dropdown-container">
-                    <label for="timeRange">Select Time Range: </label>
-                    <select id="timeRange" onchange="fetchDisk();">
-                        <option value="1" selected>1 Day</option>
-                        <option value="7">7 Days</option>
-                        <option value="30">30 Days</option>
-                        <option value="90">90 Days</option>
-                        <option value="360">1 Year</option>
-                    </select>
-                </div>
-                <canvas id="cpuChart" style="display:none;"></canvas>
-                <canvas id="memoryChart" style="display:none;"></canvas>
-                <canvas id="diskChart" style="display:none;"></canvas>
-                <div class="panel-group" id="diskPanelContainer"></div>
-            </div>
-            
-
-        </div> 
-        <footer>
-            <p class="footer">© 2024 - Cloud United Team</p>
-        </footer>
-
-        <script src="chartsOrganizer.js"></script>
-        <script>
-            const urlParams = new URLSearchParams(window.location.search);
-            const id = urlParams.get("id");
-
-            const baslik = document.getElementById("baslik");
-            baslik.textContent = id;
-
-            const addClickListener = (elementId, url) => {
-                document.getElementById(elementId).addEventListener("click", function () {
-                    window.location.href = `${url}?id=${this.textContent}`;
-                });
-            };
-
-            addClickListener("host", "hostscreen.aspx");
-            addClickListener("cluster", "clusterscreen.aspx");
-
-            function sortTable(columnIndex, column) {
-                const table = column.closest('table');
-                let rows = Array.from(table.rows).slice(1);
-                let dir = "asc", switchcount = 0, switching = true;
-
-                while (switching) {
-                    switching = false;
-                    for (let i = 0; i < rows.length - 1; i++) {
-                        let xValue = rows[i].cells[columnIndex].innerText;
-                        let yValue = rows[i + 1].cells[columnIndex].innerText;
-
-                        if (!isNaN(xValue) && !isNaN(yValue)) {
-                            xValue = parseFloat(xValue);
-                            yValue = parseFloat(yValue);
-                        }
-
-                        if ((dir === "asc" && xValue > yValue) || (dir === "desc" && xValue < yValue)) {
-                            [rows[i], rows[i + 1]] = [rows[i + 1], rows[i]];
-                            switching = true;
-                            switchcount++;
-                            break;
-                        }
-                    }
-                    if (!switchcount && dir === "asc") {
-                        dir = "desc";
-                        switching = true;
-                    }
+                if (newToken != null)
+                {
+                    var newTokenInfo = new TokenInfo
+                    {
+                        Token = newToken,
+                        ExpiryDate = DateTime.Now.Add(TokenLifetime)
+                    };
+                    StoreTokenInfo(tokenFilePath, tokenType, newTokenInfo);
+                    return newToken;
                 }
-                table.tBodies[0].append(...rows);
+
+                return null;
+            }
+            tokenInfo = ReadTokenInfoFromFile(tokenFilePath, tokenType);
+            return tokenInfo.Token;
+        }
+
+        private async Task<string> AcquireTokenAsync(string vropsServer)
+        {
+            var requestUri = $"{vropsServer}/suite-api/api/auth/token/acquire?_no_links=true";
+            var requestBody = new
+            {
+                username = _username,
+                password = _password
+            };
+            var jsonContent = new JavaScriptSerializer().Serialize(requestBody);
+            var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync(requestUri, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string tokenXml = await response.Content.ReadAsStringAsync();
+                return string.IsNullOrWhiteSpace(tokenXml) ? null : ExtractTokenFromXml(tokenXml);
             }
 
-            function openTab(panelId, button) {
-                event.preventDefault();
-                document.querySelectorAll(".columnRight").forEach(content => content.style.display = "none");
-                document.querySelectorAll(".tab-button").forEach(tabButton => tabButton.classList.remove("active"));
+            return null;
+        }
 
-                document.getElementById(panelId).style.display = "flex";
-                button.classList.add("active");
+        private static string ExtractTokenFromXml(string xmlData)
+        {
+            var xdoc = XDocument.Parse(xmlData);
+            var tokenElement = xdoc.Descendants(XName.Get("token", "http://webservice.vmware.com/vRealizeOpsMgr/1.0/")).FirstOrDefault();
+            return tokenElement?.Value;
+        }
+
+        private TokenInfo ReadTokenInfoFromFile(string filePath, string tokenType)
+        {
+            if (!File.Exists(filePath))
+            {
+                return new TokenInfo { Token = null, ExpiryDate = DateTime.MinValue };
             }
-            
-        </script>
-        <script>
-            const diskCtx = document.getElementById('diskChart').getContext('2d');
-            let diskDataMap = {};
-            let diskChart;
-            const updateDiskPanels = (diskLabels) => {
-                const container = document.getElementById('diskPanelContainer');
-                container.innerHTML = '';
 
-                diskLabels.forEach(label => {
-                    const panel = document.createElement('div');
-                    panel.className = 'disk-panel';
-                    panel.setAttribute('data-disk', label);
+            var lines = File.ReadAllLines(filePath);
+            var tokenLine = lines.FirstOrDefault(line => line.StartsWith($"{tokenType}:Token="));
+            var expiryLine = lines.FirstOrDefault(line => line.StartsWith($"{tokenType}:Expiry="));
 
-                    const averageUsage = calculateAverage(diskDataMap[label]);
+            string token = tokenLine?.Substring(tokenLine.IndexOf('=') + 1);
+            DateTime expiryDate = DateTime.TryParse(expiryLine?.Substring(expiryLine.IndexOf('=') + 1), out DateTime result) ? result : DateTime.MinValue;
 
-                    panel.innerHTML = 
-                        `${label}: ${averageUsage}%`;
+            return new TokenInfo { Token = token, ExpiryDate = expiryDate };
+        }
 
-                    panel.addEventListener('click', () => handlePanelClick(label));
-                    container.appendChild(panel);
-                });
-            };
-            const updateDiskChart = (diskLabel) => {
-                const diskData = diskDataMap[diskLabel];
-                const filteredDiskData = diskData.slice(-filteredDates.length);
-                const processedDiskData = processLargeData(filteredDiskData, filteredDates);
-                if (diskChart) diskChart.destroy();
-                if (processedDiskData.data.length>0)
-                    diskChart = createChart(diskCtx, processedDiskData.data, processedDiskData.dates, `Disk (${diskLabel}) Usage`, 'rgba(255, 159, 64, 1)');
-            };
-            const handlePanelClick = (diskLabel) => {
-                document.querySelectorAll('.disk-panel').forEach(panel => {
-                    panel.classList.remove('active');
-                });
-                document.querySelector(`.disk-panel[data-disk="${diskLabel}"]`).classList.add('active');
-                updateDiskChart(diskLabel);
-            };
+        private void StoreTokenInfo(string filePath, string tokenType, TokenInfo tokenInfo)
+        {
+            var lines = File.Exists(filePath) ? File.ReadAllLines(filePath).ToList() : new List<string>();
 
-            const calculateAverage = (data) => {
-                const sum = data.reduce((acc, d) => acc + d, 0);
-                return (sum / data.length).toFixed(2); 
-            };
+            var tokenLine = $"{tokenType}:Token={tokenInfo.Token}";
+            var expiryLine = $"{tokenType}:Expiry={tokenInfo.ExpiryDate:o}";
 
-            function fetchDisk() {
-                fetchData();
-                const diskLabels = Object.keys(diskDataMap);
-                diskLabels.sort();
-                updateDiskPanels(diskLabels);
-                if (diskLabels.length > 0) 
-                    handlePanelClick(diskLabels[0]);
+            lines.RemoveAll(line => line.StartsWith($"{tokenType}:Token=") || line.StartsWith($"{tokenType}:Expiry="));
+            lines.Add(tokenLine);
+            lines.Add(expiryLine);
 
-            }
-        </script>
-        
+            File.WriteAllLines(filePath, lines);
+        }
 
-    </form>
-</body>
-</html>
+        private string GetFilePath()
+        {
+            return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, TokenFileName);
+        }
+    }
+
+    public class TokenInfo
+    {
+        public string Token { get; set; }
+        public DateTime ExpiryDate { get; set; }
+    }
+}
+
+ 'The process cannot access the file 'C:\inetpub\wwwroot\vminfo\vminfo\tokens.txt' because it is being used by another process.'
