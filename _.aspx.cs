e2612355bae0c2cd98ef6c1a3716466475a357d0
ali candar <118173006+coolwli@users.noreply.github.com>
2024-08-22 09:52:34 +0300

    Update _.aspx.cs

--- _.aspx.cs
+++ _.aspx.cs
@@ -1,57 +1,4 @@
-using System;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Collections.Generic;
-using System.Web.UI;
-using System.Web.Script.Serialization;
-
-namespace windows_users
-{
-    public partial class _default : Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            string folderPath = @"F:\Ali\windows users";
-
-            try
-            {
-                var csvFiles = Directory.GetFiles(folderPath, "*.csv");
-
-                if (csvFiles.Length == 0)
-                {
-                    Response.Write("CSV dosyası bulunamadı.");
-                    return;
-                }
-
-                string latestFile = csvFiles.OrderByDescending(f => new FileInfo(f).CreationTime).First();
-                List<string[]> rows = new List<string[]>();
-
-                using (StreamReader reader = new StreamReader(latestFile))
-                {
-                    string line;
-                    while ((line = reader.ReadLine()) != null)
-                    {
-                        string[] columns = line.Split(',');
-                        if (columns.Length > 0)
-                        {
-                            string[] cell = columns[0].Split('+').Select(c=>c.Trim()).ToArray();
-                            rows.Add(cell);
-                        }
-                    }
-                }
-                JavaScriptSerializer serializer = new JavaScriptSerializer();
-                serializer.MaxJsonLength = Int32.MaxValue;
-                string json = serializer.Serialize(rows);
-                string script = $"<script> data = {json};initializeTable();</script>";
-                ClientScript.RegisterStartupScript(this.GetType(), "initializeData", script);
-            }
-            catch (Exception ex)
-            {
-                Response.Write("Hata: " + ex.Message);
-            }
-        }
-        protected void hiddenButton_Click(object sender, EventArgs e)
+protected void hiddenButton_Click(object sender, EventArgs e)
         {
             string jsonData = hiddenField.Value;
 
@@ -59,16 +6,32 @@ namespace windows_users
             {
                 JavaScriptSerializer js = new JavaScriptSerializer();
                 js.MaxJsonLength = int.MaxValue; // Maksimum JSON uzunluğunu ayarla
-                var tableData = js.Deserialize<List<Dictionary<string, string>>>(jsonData);
+                var tableData = js.Deserialize<List<string[]>>(jsonData);
 
+                if (tableData.Count == 0)
+                {
+                    Response.Write("No data available.");
+                    return;
+                }
+
+                // Kolon başlıklarını belirle
                 StringBuilder csv = new StringBuilder();
                 csv.AppendLine("Host,User");
 
+                // Her bir satırı CSV formatına dönüştür
                 foreach (var row in tableData)
                 {
-                    csv.AppendLine($"{row["a"]},{row["vCenter"]}");
+                    if (row.Length >= 2)
+                    {
+                        csv.AppendLine($"{row[0]},{row[1]}");
+                    }
+                    else
+                    {
+                        csv.AppendLine($"{row[0]},"); // User bilgisi yoksa boş bırak
+                    }
                 }
 
+                // CSV'yi kullanıcıya sun
                 Response.Clear();
                 Response.ContentType = "text/csv";
                 Response.AddHeader("content-disposition", "attachment;filename=vmsData.csv");
@@ -76,5 +39,3 @@ namespace windows_users
                 Response.End();
             }
         }
-    }
-}
