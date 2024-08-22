e62f8a1966431152cde11c50dc3136d83c77b448
ali candar <118173006+coolwli@users.noreply.github.com>
2024-08-22 07:39:16 +0300

    Update _.aspx.cs

--- _.aspx.cs
+++ _.aspx.cs
@@ -1,162 +1,55 @@
 using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
-using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
-using System.Web.Configuration;
-using System.Web.Script.Serialization;
-using System.Xml.Linq;
 
-namespace vminfo
+public partial class Default : System.Web.UI.Page
 {
-    public class TokenManager
+    protected void Page_Load(object sender, EventArgs e)
     {
-        private const string ConnectionStringName = "SqlConnectionString"; // app.config'de tanımlı olmalı
-        private static readonly TimeSpan TokenLifetime = TimeSpan.FromHours(3);
-
-        private readonly HttpClient _httpClient;
-        private readonly string _username;
-        private readonly string _password;
-
-        public TokenManager(HttpClient httpClient)
+        // CSV dosyalarının bulunduğu klasör
+        string folderPath = Server.MapPath("~/App_Data/");
+        
+        // Klasördeki tüm CSV dosyalarını alın
+        var csvFiles = Directory.GetFiles(folderPath, "*.csv");
+        
+        if (csvFiles.Length == 0)
         {
-            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
-            _username = WebConfigurationManager.AppSettings["VropsUsername"]
-                        ?? throw new ArgumentNullException(nameof(_username));
-            _password = WebConfigurationManager.AppSettings["VropsPassword"]
-                        ?? throw new ArgumentNullException(nameof(_password));
+            LiteralData.Text = "Klasörde CSV dosyası bulunamadı.";
+            return;
         }
+        
+        // En son eklenen dosyayı bul
+        string latestFile = csvFiles.OrderByDescending(f => new FileInfo(f).CreationTime).First();
+        
+        // En son eklenen CSV dosyasını okuyun ve ilk sütundaki verileri alın
+        StringBuilder sb = new StringBuilder();
 
-        public async Task<string> GetTokenAsync(string vcenter)
+        try
         {
-            if (vcenter == "apgaraavcs801" || vcenter == "apgartksvcs801" || vcenter == "ptekvcsd01")
+            using (StreamReader reader = new StreamReader(latestFile))
             {
-                return await CheckTokenAsync("https://apgaravrops801.fw.garanti.com.tr", "ankara");
-            }
-            return await CheckTokenAsync("https://ptekvrops01.fw.garanti.com.tr", "pendik");
-        }
-
-        private async Task<string> CheckTokenAsync(string vropsServer, string tokenType)
-        {
-            var tokenInfo = await ReadTokenInfoFromDatabaseAsync(tokenType);
-
-            if (DateTime.Now >= tokenInfo.ExpiryDate)
-            {
-                string newToken = await AcquireTokenAsync(vropsServer);
-
-                if (newToken != null)
+                string line;
+                while ((line = reader.ReadLine()) != null)
                 {
-                    var newTokenInfo = new TokenInfo
+                    // Satırı virgülle ayırarak sütunları elde edin
+                    string[] columns = line.Split(',');
+
+                    // İlk sütundaki veriyi alın
+                    if (columns.Length > 0)
                     {
-                        Token = newToken,
-                        ExpiryDate = DateTime.Now.Add(TokenLifetime)
-                    };
-                    await StoreTokenInfoToDatabaseAsync(tokenType, newTokenInfo);
-                    return newToken;
-                }
-
-                return null;
-            }
-
-            return tokenInfo.Token;
-        }
-
-        private async Task<string> AcquireTokenAsync(string vropsServer)
-        {
-            var requestUri = $"{vropsServer}/suite-api/api/auth/token/acquire?_no_links=true";
-            var requestBody = new
-            {
-                username = _username,
-                password = _password
-            };
-            var jsonContent = new JavaScriptSerializer().Serialize(requestBody);
-            var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
-
-            var response = await _httpClient.PostAsync(requestUri, content);
-
-            if (response.IsSuccessStatusCode)
-            {
-                string tokenXml = await response.Content.ReadAsStringAsync();
-                return string.IsNullOrWhiteSpace(tokenXml) ? null : ExtractTokenFromXml(tokenXml);
-            }
-
-            return null;
-        }
-
-        private static string ExtractTokenFromXml(string xmlData)
-        {
-            var xdoc = XDocument.Parse(xmlData);
-            var tokenElement = xdoc.Descendants(XName.Get("token", "http://webservice.vmware.com/vRealizeOpsMgr/1.0/")).FirstOrDefault();
-            return tokenElement?.Value;
-        }
-
-        private async Task<TokenInfo> ReadTokenInfoFromDatabaseAsync(string tokenType)
-        {
-            var tokenInfo = new TokenInfo { Token = null, ExpiryDate = DateTime.MinValue };
-            var connectionString = WebConfigurationManager.ConnectionStrings[ConnectionStringName]?.ConnectionString;
-
-            if (string.IsNullOrEmpty(connectionString))
-                throw new InvalidOperationException("Connection string is not configured.");
-
-            using (var connection = new SqlConnection(connectionString))
-            {
-                var query = "SELECT Token, ExpiryDate FROM TokenInfo WHERE TokenType = @TokenType";
-                var command = new SqlCommand(query, connection);
-                command.Parameters.AddWithValue("@TokenType", tokenType);
-
-                await connection.OpenAsync();
-                using (var reader = await command.ExecuteReaderAsync())
-                {
-                    if (await reader.ReadAsync())
-                    {
-                        tokenInfo.Token = reader["Token"] as string;
-                        tokenInfo.ExpiryDate = reader.GetDateTime(reader.GetOrdinal("ExpiryDate"));
+                        sb.AppendLine(columns[0]);
                     }
                 }
             }
 
-            return tokenInfo;
+            // Verileri Literal kontrolüne atayın
+            LiteralData.Text = sb.ToString().Replace(Environment.NewLine, "<br/>");
         }
-
-        private async Task StoreTokenInfoToDatabaseAsync(string tokenType, TokenInfo tokenInfo)
+        catch (Exception ex)
         {
-            var connectionString = WebConfigurationManager.ConnectionStrings[ConnectionStringName]?.ConnectionString;
-
-            if (string.IsNullOrEmpty(connectionString))
-                throw new InvalidOperationException("Connection string is not configured.");
-
-            using (var connection = new SqlConnection(connectionString))
-            {
-                var query = @"
-                    IF EXISTS (SELECT 1 FROM TokenInfo WHERE TokenType = @TokenType)
-                    BEGIN
-                        UPDATE TokenInfo
-                        SET Token = @Token, ExpiryDate = @ExpiryDate
-                        WHERE TokenType = @TokenType
-                    END
-                    ELSE
-                    BEGIN
-                        INSERT INTO TokenInfo (TokenType, Token, ExpiryDate)
-                        VALUES (@TokenType, @Token, @ExpiryDate)
-                    END";
-
-                var command = new SqlCommand(query, connection);
-                command.Parameters.AddWithValue("@TokenType", tokenType);
-                command.Parameters.AddWithValue("@Token", (object)tokenInfo.Token ?? DBNull.Value);
-                command.Parameters.AddWithValue("@ExpiryDate", tokenInfo.ExpiryDate);
-
-                await connection.OpenAsync();
-                await command.ExecuteNonQueryAsync();
-            }
+            // Hata durumunda uygun bir mesaj gösterin
+            LiteralData.Text = "Hata: " + ex.Message;
         }
-    }
-
-    public class TokenInfo
-    {
-        public string Token { get; set; }
-        public DateTime ExpiryDate { get; set; }
     }
 }
