4af24471d93c91355913b1fd9bf5771744634e98
ali candar <118173006+coolwli@users.noreply.github.com>
2024-08-27 22:44:05 +0300

    Update _.aspx.cs

--- _.aspx.cs
+++ _.aspx.cs
@@ -1,6 +1,84 @@
-System.Data.SqlClient.SqlException
-  HResult=0x80131904
-  Message=The incoming request has too many parameters. The server supports a maximum of 2100 parameters. Reduce the number of parameters and resend the request.
-  Source=.Net SqlClient Data Provider
-  StackTrace:
-<Cannot evaluate the exception stack trace>
+protected void hiddenButton_Click(object sender, EventArgs e)
+{
+    var jsonData = hiddenField.Value;
+
+    if (string.IsNullOrEmpty(jsonData))
+    {
+        Response.Write("No data received.");
+        Response.End();
+        return;
+    }
+
+    var serializer = new JavaScriptSerializer { MaxJsonLength = Int32.MaxValue };
+    var vmNames = serializer.Deserialize<string[]>(jsonData);
+
+    if (vmNames.Length == 0)
+    {
+        Response.Write("No VM names provided.");
+        Response.End();
+        return;
+    }
+
+    var csv = new StringBuilder();
+
+    // Define the batch size
+    int batchSize = 2000;
+    for (int batchStart = 0; batchStart < vmNames.Length; batchStart += batchSize)
+    {
+        // Get the current batch of VM names
+        var batch = vmNames.Skip(batchStart).Take(batchSize).ToArray();
+
+        // Build query for the current batch
+        var queryBuilder = new StringBuilder("SELECT * FROM vminfoVMs WHERE VMName IN (");
+        for (int i = 0; i < batch.Length; i++)
+        {
+            if (i > 0) queryBuilder.Append(", ");
+            queryBuilder.Append("@name" + i);
+        }
+        queryBuilder.Append(")");
+
+        using (var con = new SqlConnection(connectionString))
+        {
+            con.Open();
+
+            using (var cmd = new SqlCommand(queryBuilder.ToString(), con))
+            {
+                for (int i = 0; i < batch.Length; i++)
+                {
+                    cmd.Parameters.AddWithValue("@name" + i, batch[i]);
+                }
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    // Append the header row if it's the first batch
+                    if (batchStart == 0)
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            if (i > 0) csv.Append(",");
+                            csv.Append(reader.GetName(i));
+                        }
+                        csv.AppendLine();
+                    }
+
+                    // Append data rows
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            if (i > 0) csv.Append(",");
+                            csv.Append(reader.IsDBNull(i) ? string.Empty : reader.GetValue(i).ToString());
+                        }
+                        csv.AppendLine();
+                    }
+                }
+            }
+        }
+    }
+
+    Response.Clear();
+    Response.ContentType = "text/csv";
+    Response.AddHeader("content-disposition", "attachment;filename=vmsData.csv");
+    Response.Write(csv.ToString());
+    Response.End();
+}
