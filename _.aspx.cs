96fff85ebda30f35af4a9d56ddc84d4073cb2003
ali candar <118173006+coolwli@users.noreply.github.com>
2024-08-28 09:52:42 +0300

    Update _.aspx.cs

--- _.aspx.cs
+++ _.aspx.cs
@@ -1,82 +1,79 @@
-protected void hiddenButton_Click(object sender, EventArgs e)
-{
-    var jsonData = hiddenField.Value;
 
-    if (string.IsNullOrEmpty(jsonData))
-    {
-        Response.Write("No data received.");
-        Response.End();
-        return;
-    }
-
-    var serializer = new JavaScriptSerializer { MaxJsonLength = Int32.MaxValue };
-    var vmNames = serializer.Deserialize<string[]>(jsonData);
-
-    if (vmNames.Length == 0)
-    {
-        Response.Write("No VM names provided.");
-        Response.End();
-        return;
-    }
-
-    var csv = new StringBuilder();
-    int batchSize = 2000; // Maximum number of parameters allowed per query
-    for (int batchStart = 0; batchStart < vmNames.Length; batchStart += batchSize)
-    {
-        // Get a batch of VM names
-        var batch = vmNames.Skip(batchStart).Take(batchSize).ToArray();
-
-        // Build SQL query for the current batch
-        var queryBuilder = new StringBuilder("SELECT * FROM vminfoVMs WHERE VMName IN (");
-        for (int i = 0; i < batch.Length; i++)
+        protected void hiddenButton_Click(object sender, EventArgs e)
         {
-            if (i > 0) queryBuilder.Append(", ");
-            queryBuilder.Append("@name" + i);
-        }
-        queryBuilder.Append(")");
+            var jsonData = hiddenField.Value;
 
-        using (var con = new SqlConnection(connectionString))
-        {
-            con.Open();
-
-            using (var cmd = new SqlCommand(queryBuilder.ToString(), con))
+            if (string.IsNullOrEmpty(jsonData))
             {
+                Response.Write("No data received.");
+                Response.End();
+                return;
+            }
+
+            var serializer = new JavaScriptSerializer { MaxJsonLength = Int32.MaxValue };
+            var vmNames = serializer.Deserialize<string[]>(jsonData);
+
+            if (vmNames.Length == 0)
+            {
+                Response.Write("No VM names provided.");
+                Response.End();
+                return;
+            }
+
+            var csv = new StringBuilder();
+            int batchSize = 2000; 
+            for (int batchStart = 0; batchStart < vmNames.Length; batchStart += batchSize)
+            {
+                var batch = vmNames.Skip(batchStart).Take(batchSize).ToArray();
+
+                var queryBuilder = new StringBuilder("SELECT * FROM vminfoVMs WHERE VMName IN (");
                 for (int i = 0; i < batch.Length; i++)
                 {
-                    cmd.Parameters.AddWithValue("@name" + i, batch[i]);
+                    if (i > 0) queryBuilder.Append(", ");
+                    queryBuilder.Append("@name" + i);
                 }
+                queryBuilder.Append(")");
 
-                using (var reader = cmd.ExecuteReader())
+                using (var con = new SqlConnection(connectionString))
                 {
-                    // Append the header row if it's the first batch
-                    if (batchStart == 0)
-                    {
-                        for (int i = 0; i < reader.FieldCount; i++)
-                        {
-                            if (i > 0) csv.Append(",");
-                            csv.Append(reader.GetName(i));
-                        }
-                        csv.AppendLine();
-                    }
+                    con.Open();
 
-                    // Append data rows
-                    while (reader.Read())
+                    using (var cmd = new SqlCommand(queryBuilder.ToString(), con))
                     {
-                        for (int i = 0; i < reader.FieldCount; i++)
+                        for (int i = 0; i < batch.Length; i++)
                         {
-                            if (i > 0) csv.Append(",");
-                            csv.Append(reader.IsDBNull(i) ? string.Empty : reader.GetValue(i).ToString());
+                            cmd.Parameters.AddWithValue("@name" + i, batch[i]);
                         }
-                        csv.AppendLine();
+
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            if (batchStart == 0)
+                            {
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    if (i > 0) csv.Append(",");
+                                    csv.Append(reader.GetName(i));
+                                }
+                                csv.AppendLine();
+                            }
+
+                            while (reader.Read())
+                            {
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    if (i > 0) csv.Append(",");
+                                    csv.Append(reader.IsDBNull(i) ? string.Empty : reader.GetValue(i).ToString());
+                                }
+                                csv.AppendLine();
+                            }
+                        }
                     }
                 }
             }
-        }
-    }
 
-    Response.Clear();
-    Response.ContentType = "text/csv";
-    Response.AddHeader("content-disposition", "attachment;filename=vmsData.csv");
-    Response.Write(csv.ToString());
-    Response.End();
-}
+            Response.Clear();
+            Response.ContentType = "text/csv";
+            Response.AddHeader("content-disposition", "attachment;filename=vmpedia_vms.csv");
+            Response.Write(csv.ToString());
+            Response.End();
+        }
