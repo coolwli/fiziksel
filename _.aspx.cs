43f59459626c52daf2876a3730c25e34f0e7d128
ali candar <118173006+coolwli@users.noreply.github.com>
2024-09-19 17:24:29 +0300

    Update _.aspx.cs

--- _.aspx.cs
+++ _.aspx.cs
@@ -1,173 +1,87 @@
 using System;
-using System.Data.SqlClient;
-using System.Net.Http;
+using System.IO;
+using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Web.Configuration;
+using System.Collections.Generic;
+using System.Web.UI;
 using System.Web.Script.Serialization;
-using System.Xml.Linq;
 
-namespace Vmpedia
+namespace windows_users
 {
-    public class TokenManager
+    public partial class local : System.Web.UI.Page
     {
-        private static readonly TimeSpan TokenLifetime = TimeSpan.FromHours(3);
-
-        private readonly HttpClient _httpClient;
-        private readonly string _username;
-        private readonly string _password;
-
-        public TokenManager(HttpClient httpClient)
+        protected void Page_Load(object sender, EventArgs e)
         {
-            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
-            _username = WebConfigurationManager.AppSettings["VropsUsername"]
-                        ?? throw new ArgumentNullException(nameof(_username));
-            _password = WebConfigurationManager.AppSettings["VropsPassword"]
-                        ?? throw new ArgumentNullException(nameof(_password));
-        }
+            string folderPath = @"C:\temp\windows users\local users";
 
-        public async Task<string> GetTokenAsync(string vcenter)
-        {
-            var vropsServer = GetVropsServer(vcenter);
-            var tokenType = GetTokenType(vcenter);
-
-            return await CheckTokenAsync(vropsServer, tokenType);
-        }
-
-        private string GetVropsServer(string vcenter)
-        {
-            return vcenter switch
+            try
             {
-                "apgaraavcs801" or "apgartksvcs801" or "ptekvcsd01" => "https://apgaravrops801.fw.garanti.com.tr",
-                _ => "https://ptekvrops01.fw.garanti.com.tr",
-            };
-        }
+                var csvFiles = Directory.GetFiles(folderPath, "*.csv");
 
-        private string GetTokenType(string vcenter)
-        {
-            return vcenter switch
-            {
-                "apgaraavcs801" or "apgartksvcs801" => "ankara",
-                _ => "pendik",
-            };
-        }
-
-        private async Task<string> CheckTokenAsync(string vropsServer, string tokenType)
-        {
-            var tokenInfo = await ReadTokenInfoFromDatabaseAsync(tokenType);
-
-            if (DateTime.Now >= tokenInfo.ExpiryDate)
-            {
-                string newToken = await AcquireTokenAsync(vropsServer);
-
-                if (newToken != null)
+                if (csvFiles.Length == 0)
                 {
-                    var newTokenInfo = new TokenInfo
-                    {
-                        Token = newToken,
-                        ExpiryDate = DateTime.Now.Add(TokenLifetime)
-                    };
-                    await StoreTokenInfoToDatabaseAsync(tokenType, newTokenInfo);
-                    return newToken;
+                    Response.Write("CSV dosyası bulunamadı.");
+                    return;
                 }
 
-                return null;
-            }
+                string latestFile = csvFiles.OrderByDescending(f => new FileInfo(f).CreationTime).First();
+                List<string[]> rows = new List<string[]>();
 
-            // Extend the existing token's lifetime
-            var extendedTokenInfo = new TokenInfo
-            {
-                Token = tokenInfo.Token,
-                ExpiryDate = DateTime.Now.Add(TokenLifetime)
-            };
-            await StoreTokenInfoToDatabaseAsync(tokenType, extendedTokenInfo);
-
-            return tokenInfo.Token;
-        }
-
-        private async Task<string> AcquireTokenAsync(string vropsServer)
-        {
-            var requestUri = $"{vropsServer}/suite-api/api/auth/token/acquire?_no_links=true";
-            var requestBody = new
-            {
-                username = _username,
-                password = _password
-            };
-            var jsonContent = new JavaScriptSerializer().Serialize(requestBody);
-            var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
-
-            var response = await _httpClient.PostAsync(requestUri, content);
-
-            if (response.IsSuccessStatusCode)
-            {
-                string tokenXml = await response.Content.ReadAsStringAsync();
-                return string.IsNullOrWhiteSpace(tokenXml) ? null : ExtractTokenFromXml(tokenXml);
-            }
-
-            return null;
-        }
-
-        private static string ExtractTokenFromXml(string xmlData)
-        {
-            var xdoc = XDocument.Parse(xmlData);
-            var tokenElement = xdoc.Descendants(XName.Get("token", "http://webservice.vmware.com/vRealizeOpsMgr/1.0/")).FirstOrDefault();
-            return tokenElement?.Value;
-        }
-
-        private async Task<TokenInfo> ReadTokenInfoFromDatabaseAsync(string tokenType)
-        {
-            var connectionString = @"Data Source=TEKSCR1\SQLEXPRESS;Initial Catalog=CloudUnited;Integrated Security=True";
-
-            using var connection = new SqlConnection(connectionString);
-            var query = "SELECT Token, ExpiryDate FROM TokenInfo WHERE TokenType = @TokenType";
-            using var command = new SqlCommand(query, connection);
-            command.Parameters.AddWithValue("@TokenType", tokenType);
-
-            await connection.OpenAsync();
-            using var reader = await command.ExecuteReaderAsync();
-            if (await reader.ReadAsync())
-            {
-                return new TokenInfo
+                using (StreamReader reader = new StreamReader(latestFile))
                 {
-                    Token = reader["Token"] as string,
-                    ExpiryDate = reader.GetDateTime(reader.GetOrdinal("ExpiryDate"))
-                };
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] columns = line.Split(',');
+                        if (columns.Length > 0)
+                        {
+                            string[] cell = columns[0].Split('+').Select(c => c.Trim()).ToArray();
+                            rows.Add(cell);
+                        }
+                    }
+                }
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                serializer.MaxJsonLength = Int32.MaxValue;
+                string json = serializer.Serialize(rows);
+                string script = $"<script> data = {json};initializeTable();</script>";
+                ClientScript.RegisterStartupScript(this.GetType(), "initializeData", script);
             }
-
-            return new TokenInfo { Token = null, ExpiryDate = DateTime.MinValue };
+            catch (Exception ex)
+            {
+                Response.Write("Hata: " + ex.Message);
+            }
         }
-
-        private async Task StoreTokenInfoToDatabaseAsync(string tokenType, TokenInfo tokenInfo)
+        protected void hiddenButton_Click(object sender, EventArgs e)
         {
-            var connectionString = @"Data Source=TEKSCR1\SQLEXPRESS;Initial Catalog=CloudUnited;Integrated Security=True";
+            string jsonData = hiddenField.Value;
 
-            using var connection = new SqlConnection(connectionString);
-            var query = @"
-                IF EXISTS (SELECT 1 FROM TokenInfo WHERE TokenType = @TokenType)
-                BEGIN
-                    UPDATE TokenInfo
-                    SET Token = @Token, ExpiryDate = @ExpiryDate
-                    WHERE TokenType = @TokenType
-                END
-                ELSE
-                BEGIN
-                    INSERT INTO TokenInfo (TokenType, Token, ExpiryDate)
-                    VALUES (@TokenType, @Token, @ExpiryDate)
-                END";
+            if (!string.IsNullOrEmpty(jsonData))
+            {
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                js.MaxJsonLength = int.MaxValue;
+                var tableData = js.Deserialize<List<string[]>>(jsonData);
 
-            using var command = new SqlCommand(query, connection);
-            command.Parameters.AddWithValue("@TokenType", tokenType);
-            command.Parameters.AddWithValue("@Token", (object)tokenInfo.Token ?? DBNull.Value);
-            command.Parameters.AddWithValue("@ExpiryDate", tokenInfo.ExpiryDate);
+                if (tableData.Count == 0)
+                {
+                    Response.Write("No data available.");
+                    return;
+                }
 
-            await connection.OpenAsync();
-            await command.ExecuteNonQueryAsync();
+                StringBuilder csv = new StringBuilder();
+                csv.AppendLine("Host,User");
+
+                foreach (var row in tableData)
+                {
+                    csv.AppendLine($"{row[0]},{row[1]}");
+
+                }
+
+                Response.Clear();
+                Response.ContentType = "text/csv";
+                Response.AddHeader("content-disposition", "attachment;filename=local users.csv");
+                Response.Write(csv.ToString());
+                Response.End();
+            }
         }
-    }
-
-    public class TokenInfo
-    {
-        public string Token { get; set; }
-        public DateTime ExpiryDate { get; set; }
     }
 }
