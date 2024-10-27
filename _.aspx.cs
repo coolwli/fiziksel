21fd0a7305895ec522f5af3441a7f22aaa5c6eca
ali candar <118173006+coolwli@users.noreply.github.com>
2024-10-27 17:00:38 +0300

    Update _.aspx.cs

--- _.aspx.cs
+++ _.aspx.cs
@@ -1,21 +1,11 @@
-9646af37-b6cc-4be7-9445-3595dec7ff03
-
 using System;
 using System.Net;
-using System.IO;
-using System.Linq;
-using System.Xml;
-using System.Text;
-using System.Data;
 using System.Net.Http;
-using System.Xml.Linq;
-using System.Configuration;
-using System.Data.SqlClient;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web.Configuration;
-using System.Web.UI.WebControls;
-using System.Collections.Generic;
-using System.Web.UI.HtmlControls;
+using System.Data.SqlClient;
+using System.Xml.Linq;
 using System.Web.Script.Serialization;
 
 namespace odmvms
@@ -23,81 +13,55 @@ namespace odmvms
     public partial class _default : System.Web.UI.Page
     {
         private static readonly TimeSpan TokenLifetime = TimeSpan.FromHours(3);
-
-        private static  HttpClient _httpClient = new HttpClient { Timeout = TimeSpan.FromSeconds(60) };
+        private static readonly HttpClient _httpClient = new HttpClient { Timeout = TimeSpan.FromSeconds(60) };
+        
         private string _username;
         private string _password;
 
         static _default()
         {
-            
             ServicePointManager.ServerCertificateValidationCallback = (message, cert, chain, sslPolicyErrors) => true;
         }
+
         protected async void Page_Load(object sender, EventArgs e)
         {
             _username = WebConfigurationManager.AppSettings["VropsUsername"];
             _password = WebConfigurationManager.AppSettings["VropsPassword"];
 
-            string token = await CheckTokenAsync("https://ptekvrops01.fw.garanti.com.tr", "pendik");
-            if (token == null)
-            {
-                form1.InnerHtml = "Bir hata olustu daha sonra deneyiniz..";
-            }
-            else
-            {
-
-            }
+            string dashboardData = await CheckTokenAndFetchDashboardDataAsync("https://ptekvrops01.fw.garanti.com.tr", "pendik", "9646af37-b6cc-4be7-9445-3595dec7ff03");
+            form1.InnerHtml = dashboardData ?? "Bir hata olustu daha sonra deneyiniz..";
         }
 
-
-        private async Task<string> CheckTokenAsync(string vropsServer, string tokenType)
+        private async Task<string> CheckTokenAndFetchDashboardDataAsync(string vropsServer, string tokenType, string dashboardId)
         {
             var tokenInfo = await ReadTokenInfoFromDatabaseAsync(tokenType);
 
             if (DateTime.Now >= tokenInfo.ExpiryDate)
             {
                 string newToken = await AcquireTokenAsync(vropsServer);
-
                 if (newToken != null)
                 {
-
-                    var newTokenInfo = new TokenInfo
-                    {
-                        Token = newToken,
-                        ExpiryDate = DateTime.Now.Add(TokenLifetime)
-                    };
-                    await StoreTokenInfoToDatabaseAsync(tokenType, newTokenInfo);
-                    return newToken;
+                    await StoreTokenInfoToDatabaseAsync(tokenType, new TokenInfo { Token = newToken, ExpiryDate = DateTime.Now.Add(TokenLifetime) });
+                    return await GetDashboardDataAsync(vropsServer, newToken, dashboardId);
                 }
 
                 return null;
             }
 
-
-            // Extend the existing token's lifetime
-            var extendedTokenInfo = new TokenInfo
-            {
-                Token = tokenInfo.Token,
-                ExpiryDate = DateTime.Now.Add(TokenLifetime)
-            };
+            // Extend existing token's lifetime and fetch dashboard data
+            var extendedTokenInfo = new TokenInfo { Token = tokenInfo.Token, ExpiryDate = DateTime.Now.Add(TokenLifetime) };
             await StoreTokenInfoToDatabaseAsync(tokenType, extendedTokenInfo);
-
-            return tokenInfo.Token;
+            return await GetDashboardDataAsync(vropsServer, tokenInfo.Token, dashboardId);
         }
 
         private async Task<string> AcquireTokenAsync(string vropsServer)
         {
             var requestUri = $"{vropsServer}/suite-api/api/auth/token/acquire?_no_links=true";
-            var requestBody = new
-            {
-                username = _username,
-                password = _password
-            };
+            var requestBody = new { username = _username, password = _password };
             var jsonContent = new JavaScriptSerializer().Serialize(requestBody);
             var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
 
             var response = await _httpClient.PostAsync(requestUri, content);
-
             if (response.IsSuccessStatusCode)
             {
                 string tokenXml = await response.Content.ReadAsStringAsync();
@@ -114,9 +78,23 @@ namespace odmvms
             return tokenElement?.Value;
         }
 
+        private async Task<string> GetDashboardDataAsync(string vropsServer, string token, string dashboardId)
+        {
+            var requestUri = $"{vropsServer}/suite-api/api/dashboard/{dashboardId}/data?_no_links=true";
+            _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+            var response = await _httpClient.GetAsync(requestUri);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+
+            return null; // Or handle the error appropriately
+        }
+
         private async Task<TokenInfo> ReadTokenInfoFromDatabaseAsync(string tokenType)
         {
-            var tokenInfo = new TokenInfo { Token = null, ExpiryDate = DateTime.MinValue };
+            var tokenInfo = new TokenInfo();
             var connectionString = @"Data Source=TEKSCR1\SQLEXPRESS;Initial Catalog=CloudUnited;Integrated Security=True";
 
             using (var connection = new SqlConnection(connectionString))
@@ -170,9 +148,8 @@ namespace odmvms
 
         protected void hiddenButton_Click(object sender, EventArgs e)
         {
+            // Logic for hidden button can be implemented here
         }
-
-
     }
 
     public class TokenInfo
