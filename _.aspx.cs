cc1c24cb15001a367b785d44db5e69b28266328c
ali candar <118173006+coolwli@users.noreply.github.com>
2024-11-14 20:53:31 +0300

    Update _.aspx.cs

--- _.aspx.cs
+++ _.aspx.cs
@@ -1,226 +1,89 @@
 using System;
-using System.Net;
-using System.Text;
-using System.Linq;
-using System.Web.UI;
-using System.Xml.Linq;
-using System.Net.Http;
-using System.Data.SqlClient;
-using System.Threading.Tasks;
+using System.Configuration;
 using System.Web.Configuration;
-using System.Collections.Generic;
-using System.Web.Script.Serialization;
+using System.Web.UI.WebControls;
 
-namespace odmvms
+public partial class ManageUsers : System.Web.UI.Page
 {
-    public partial class _default : Page
+    protected void Page_Load(object sender, EventArgs e)
     {
-        private static readonly TimeSpan TokenLifetime = TimeSpan.FromHours(3);
-        private static readonly HttpClient _httpClient = new HttpClient { Timeout = TimeSpan.FromSeconds(60) };
-
-        private const string OpsNamespace = "http://webservice.vmware.com/vRealizeOpsMgr/1.0/";
-        private string _username;
-        private string _password;
-
-        static _default()
+        if (!IsPostBack)
         {
-            ServicePointManager.ServerCertificateValidationCallback = (message, cert, chain, sslPolicyErrors) => true;
-        }
-
-        protected async void Page_Load(object sender, EventArgs e)
-        {
-            _username = WebConfigurationManager.AppSettings["VropsUsername"];
-            _password = WebConfigurationManager.AppSettings["VropsPassword"];
-
-            var data = await CheckTokenAndFetchDataAsync("https://ptekvrops01.fw.garanti.com.tr", "pendik");
-            if (data == null)
-            {
-                form1.InnerHtml = "Bir hata olustu, daha sonra deneyiniz..";
-            }
-            
-        }
-
-        private async Task<string> CheckTokenAndFetchDataAsync(string vropsServer, string tokenType)
-        {
-            var tokenInfo = await ReadTokenInfoAsync(tokenType);
-
-            if (tokenInfo.ExpiryDate <= DateTime.Now)
-            {
-                var newToken = await AcquireTokenAsync(vropsServer);
-                if (newToken != null)
-                {
-                    await StoreTokenInfoToDatabaseAsync(tokenType, new TokenInfo { Token = newToken, ExpiryDate = DateTime.Now.Add(TokenLifetime) });
-                    return await GetDataAsync(vropsServer, newToken);
-                }
-                return null;
-            }
-
-            var extendedTokenInfo = new TokenInfo { Token = tokenInfo.Token, ExpiryDate = DateTime.Now.Add(TokenLifetime) };
-            await StoreTokenInfoToDatabaseAsync(tokenType, extendedTokenInfo);
-            return await GetDataAsync(vropsServer, tokenInfo.Token);
-        }
-
-        private async Task<string> AcquireTokenAsync(string vropsServer)
-        {
-            var requestUri = $"{vropsServer}/suite-api/api/auth/token/acquire?_no_links=true";
-            var requestBody = new { username = _username, password = _password };
-            var jsonContent = new JavaScriptSerializer().Serialize(requestBody);
-            var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
-
-            var response = await _httpClient.PostAsync(requestUri, content);
-            if (response.IsSuccessStatusCode)
-            {
-                var tokenXml = await response.Content.ReadAsStringAsync();
-                return string.IsNullOrWhiteSpace(tokenXml) ? null : ExtractTokenFromXml(tokenXml);
-            }
-            return null;
-        }
-
-        private static string ExtractTokenFromXml(string xmlData)
-        {
-            var xdoc = XDocument.Parse(xmlData);
-            var tokenElement = xdoc.Descendants(XName.Get("token", OpsNamespace)).FirstOrDefault();
-            return tokenElement?.Value;
-        }
-
-        private async Task<string> GetDataAsync(string vropsServer, string token)
-        {
-            var getIdUrl = $"{vropsServer}/suite-api/internal/views/e5bb44f3-f7d8-45c5-8819-dfc6e7672463/data/export?resourceId=00330e14-5263-4728-8273-a135ae4d22fa&pageSize=10000&traversalSpec=vSphere Hosts and Clusters-VMWARE-vSphere World&_ack=true";
-
-            var request = new HttpRequestMessage(HttpMethod.Get, getIdUrl);
-            request.Headers.Add("Authorization", $"vRealizeOpsToken {token}");
-
-            using (var response = await _httpClient.SendAsync(request))
-            {
-                if (response.IsSuccessStatusCode)
-                {
-                    var jsonData = await response.Content.ReadAsStringAsync();
-                    return ParseDashboardData(jsonData);
-                }
-            }
-            return null;
-        }
-
-        private string ParseDashboardData(string jsonData)
-        {
-            var js = new JavaScriptSerializer { MaxJsonLength = Int32.MaxValue };
-            dynamic data = js.Deserialize<dynamic>(jsonData);
-            var tableRows = new List<Dictionary<string, object>>();
-
-            foreach (var view in data)
-            {
-                foreach (var elements in view.Value)
-                {
-                    foreach (var rows in elements["rows"])
-                    {
-                        var tableRow = new Dictionary<string, object>();
-                        foreach (var row in rows)
-                        {
-                            tableRow["name"] = row.Value["objId"];
-                            tableRow["ps"] = row.Value["1"];
-                            tableRow["ip"] = row.Value["2"];
-                            tableRow["os"] = row.Value["3"];
-                            tableRow["cl"] = row.Value["4"];
-                            tableRow["vc"] = row.Value["5"];
-                        }
-                        tableRows.Add(tableRow);
-                    }
-                }
-            }
-            var json = js.Serialize(tableRows);
-            var script = $"<script>data = {json}; initializeTable(); </script>";
-            ClientScript.RegisterStartupScript(GetType(), "initializeData", script);
-
-            return "1";
-        }
-
-        private async Task<TokenInfo> ReadTokenInfoAsync(string tokenType)
-        {
-            var tokenInfo = new TokenInfo();
-            var connectionString = @"Data Source=TEKSCR1\SQLEXPRESS;Initial Catalog=CloudUnited;Integrated Security=True";
-
-            using (var connection = new SqlConnection(connectionString))
-            {
-                var query = "SELECT Token, ExpiryDate FROM TokenInfo WHERE TokenType = @TokenType";
-                var command = new SqlCommand(query, connection);
-                command.Parameters.AddWithValue("@TokenType", tokenType);
-
-                await connection.OpenAsync();
-                using (var reader = await command.ExecuteReaderAsync())
-                {
-                    if (await reader.ReadAsync())
-                    {
-                        tokenInfo.Token = reader["Token"] as string;
-                        tokenInfo.ExpiryDate = reader.GetDateTime(reader.GetOrdinal("ExpiryDate"));
-                    }
-                }
-            }
-            return tokenInfo;
-        }
-
-        private async Task StoreTokenInfoToDatabaseAsync(string tokenType, TokenInfo tokenInfo)
-        {
-            var connectionString = @"Data Source=TEKSCR1\SQLEXPRESS;Initial Catalog=CloudUnited;Integrated Security=True";
-
-            using (var connection = new SqlConnection(connectionString))
-            {
-                var query = @"
-                    IF EXISTS (SELECT 1 FROM TokenInfo WHERE TokenType = @TokenType)
-                    BEGIN
-                        UPDATE TokenInfo SET Token = @Token, ExpiryDate = @ExpiryDate WHERE TokenType = @TokenType
-                    END
-                    ELSE
-                    BEGIN
-                        INSERT INTO TokenInfo (TokenType, Token, ExpiryDate) VALUES (@TokenType, @Token, @ExpiryDate)
-                    END";
-
-                var command = new SqlCommand(query, connection);
-                command.Parameters.AddWithValue("@TokenType", tokenType);
-                command.Parameters.AddWithValue("@Token", (object)tokenInfo.Token ?? DBNull.Value);
-                command.Parameters.AddWithValue("@ExpiryDate", tokenInfo.ExpiryDate);
-
-                await connection.OpenAsync();
-                await command.ExecuteNonQueryAsync();
-            }
-        }
-
-        protected void hiddenButton_Click(object sender, EventArgs e)
-        {
-            string jsonData = hiddenField.Value;
-
-            if (!string.IsNullOrEmpty(jsonData))
-            {
-                JavaScriptSerializer js = new JavaScriptSerializer();
-                js.MaxJsonLength = int.MaxValue;
-                var tableData = js.Deserialize<List<Dictionary<string,object>>>(jsonData);
-
-                if (tableData.Count == 0)
-                {
-                    Response.Write("No data available.");
-                    return;
-                }
-
-                StringBuilder csv = new StringBuilder();
-                csv.AppendLine("Name;Power State;IPv4;OS;Cluster;VCenter");
-
-                foreach (var row in tableData)
-                {
-                    csv.AppendLine($"{row["name"]};{row["ps"]};{row["ip"]};{row["os"]};{row["cl"]};{row["vc"]}");
-
-                }
-
-                Response.Clear();
-                Response.ContentType = "text/csv";
-                Response.AddHeader("content-disposition", "attachment;filename=Replicated VMs.csv");
-                Response.Write(csv.ToString());
-                Response.End();
-            }
+            LoadUsers();
         }
     }
 
-    public class TokenInfo
+    private Configuration GetConfig()
     {
-        public string Token { get; set; }
-        public DateTime ExpiryDate { get; set; }
+        // F dizinindeki dosya yolu
+        string path = @"F:\path\to\your\web.config";
+        var configFileMap = new ExeConfigurationFileMap { ExeConfigFilename = path };
+        return ConfigurationManager.OpenMappedExeConfiguration(configFileMap, ConfigurationUserLevel.None);
+    }
+
+    private void LoadUsers()
+    {
+        var users = new List<string>();
+        var config = GetConfig();
+        var authorizationSection = (AuthorizationSection)config.GetSection("system.web/authorization");
+
+        foreach (AuthorizationRule rule in authorizationSection.Rules)
+        {
+            if (rule.Action == AuthorizationRuleAction.Allow)
+            {
+                foreach (string user in rule.Users)
+                {
+                    users.Add(user);
+                }
+            }
+        }
+
+        UsersGridView.DataSource = users.ConvertAll(user => new { Username = user });
+        UsersGridView.DataBind();
+    }
+
+    protected void AddUserButton_Click(object sender, EventArgs e)
+    {
+        string newUser = NewUserTextBox.Text.Trim();
+        if (!string.IsNullOrEmpty(newUser))
+        {
+            var config = GetConfig();
+            var authorizationSection = (AuthorizationSection)config.GetSection("system.web/authorization");
+
+            AuthorizationRule newRule = new AuthorizationRule(AuthorizationRuleAction.Allow);
+            newRule.Users.Add(newUser);
+            authorizationSection.Rules.Add(newRule);
+
+            config.Save(ConfigurationSaveMode.Modified);
+            MessageLabel.Text = $"{newUser} eklendi.";
+            LoadUsers();
+        }
+        else
+        {
+            MessageLabel.Text = "Kullanıcı adı boş olamaz.";
+        }
+    }
+
+    protected void UsersGridView_RowCommand(object sender, GridViewCommandEventArgs e)
+    {
+        if (e.CommandName == "Remove")
+        {
+            string userToRemove = e.CommandArgument.ToString();
+            var config = GetConfig();
+            var authorizationSection = (AuthorizationSection)config.GetSection("system.web/authorization");
+
+            foreach (AuthorizationRule rule in authorizationSection.Rules)
+            {
+                if (rule.Action == AuthorizationRuleAction.Allow && rule.Users.Contains(userToRemove))
+                {
+                    rule.Users.Remove(userToRemove);
+                    break;
+                }
+            }
+
+            config.Save(ConfigurationSaveMode.Modified);
+            MessageLabel.Text = $"{userToRemove} kaldırıldı.";
+            LoadUsers();
+        }
     }
 }
