756247a18dcf7a744cbefd5cf5b948b7ffa318fd
ali candar <118173006+coolwli@users.noreply.github.com>
2025-01-27 17:46:13 +0300

    Update _.aspx.cs

--- _.aspx.cs
+++ _.aspx.cs
@@ -1,67 +1,65 @@
-        protected void Page_Load(object sender, EventArgs e)
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.UI;
+using System.Web.Script.Serialization;
+using Microsoft.Office.Interop.Excel;
+
+public partial class Default : Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string sharedFolderPath = @"\\tekscr1\f$\Serhat\RvTools_Inventory\Reports";  
+
+        // Klasördeki tüm Excel dosyalarını al (xlsx uzantılı)
+        var excelFiles = Directory.GetFiles(sharedFolderPath, "*.xlsx")
+                                   .Select(file => new FileInfo(file))
+                                   .OrderByDescending(file => file.LastWriteTime)  // En son değiştirilene göre sırala
+                                   .Take(4)  // En son 4 dosyayı al
+                                   .ToList();
+
+        // Dosya listesi boşsa hata mesajı ver
+        if (excelFiles.Count == 0)
         {
-            string sharedFolderPath = @"\\tekscr1\f$\Serhat\RvTools_Inventory\Reports";  
-
-            // Klasördeki tüm Excel dosyalarını al (xlsx uzantılı)
-            var excelFiles = Directory.GetFiles(sharedFolderPath, "*.xlsx")
-                                       .Select(file => new FileInfo(file))
-                                       .OrderByDescending(file => file.LastWriteTime)  // En son değiştirilene göre sırala
-                                       .Take(4)  // En son 4 dosyayı al
-                                       .ToList();
-
-            // Dosya listesi boşsa hata mesajı ver
-            if (excelFiles.Count == 0)
-            {
-                Response.Write("Paylaşımlı klasörde hiç Excel dosyası bulunamadı.");
-                return;
-            }
-
-            // Excel dosyalarını tek tek oku
-            foreach (var file in excelFiles)
-            {
-                string excelFilePath = file.FullName;  // Dosyanın tam yolu
-                string connectionString =
-                    @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + excelFilePath + @";Extended Properties=""Excel 12.0 Xml;HDR=YES;""";
-
-                OleDbConnection connection = new OleDbConnection(connectionString);
-
-                try
-                {
-                    connection.Open();
-                    Response.Write("aldiö");
-                    return;
-                    // Belirli bir sayfayı seçin (Örneğin, "Sheet1")
-                    string query = "SELECT [Column1], [Column2], [Column3] FROM [Sheet1$]";  // Verileri alacağınız sayfa
-
-                    OleDbDataAdapter dataAdapter = new OleDbDataAdapter(query, connection);
-                    DataTable dt = new DataTable();
-                    dataAdapter.Fill(dt);
-
-                    // Dosya adı ve veriyi ekrana yazdırma
-                    Response.Write("<b>Dosya Adı: </b>" + file.Name + "<br/>");
-
-                    // Veriyi ekranda yazdırma
-                    foreach (DataRow row in dt.Rows)
-                    {
-                        Response.Write(row["Column1"] + " - " + row["Column2"] + " - " + row["Column3"] + "<br/>");
-                    }
-
-                    Response.Write("<br/>");
-                }
-                catch (Exception ex)
-                {
-                    Response.Write("Hata: " + ex.Message);
-                }
-                finally
-                {
-                    connection.Close();
-                }
-            }
+            Response.Write("Paylaşımlı klasörde hiç Excel dosyası bulunamadı.");
+            return;
         }
-        office intertop excell
-                List<string[]> rows = new List<string[]>();
+
+        // Excel dosyalarını tek tek oku
+        List<string[]> rows = new List<string[]>();
+
+        foreach (var file in excelFiles)
+        {
+            string excelFilePath = file.FullName;  // Dosyanın tam yolu
+
+            Application excelApp = new Application();
+            Workbook workbook = excelApp.Workbooks.Open(excelFilePath);
+            Worksheet worksheet = (Worksheet)workbook.Sheets[1]; // İlk sayfayı al
+
+            Range range = worksheet.UsedRange;
+            int rowCount = range.Rows.Count;
+
+            // Excel dosyasındaki verileri oku
+            for (int i = 1; i <= rowCount; i++)
+            {
+                string[] row = new string[3]; // 3 sütun olduğunu varsayıyoruz
+                row[0] = range.Cells[i, 1].Value2.ToString();
+                row[1] = range.Cells[i, 2].Value2.ToString();
+                row[2] = range.Cells[i, 3].Value2.ToString();
+                rows.Add(row);
+            }
+
+            // Çalışma kitabını ve Excel uygulamasını kapat
+            workbook.Close(0);
+            excelApp.Quit();
+        }
+
+        // JavaScript'e veri göndermek için JSON formatına dönüştür
         JavaScriptSerializer serializer = new JavaScriptSerializer();
         serializer.MaxJsonLength = Int32.MaxValue;
-                string json = serializer.Serialize(rows);
-        string script = $"<script> data = {json};initializeTable();</script>";
+        string json = serializer.Serialize(rows);
+        string script = $"<script>data = {json}; initializeTable();</script>";
+
         ClientScript.RegisterStartupScript(this.GetType(), "initializeData", script);
+    }
+}
