19fd0b10e2477bd71cdd85f0ebddf690e975817d
ali candar <118173006+coolwli@users.noreply.github.com>
2025-04-15 09:55:25 +0300

    Update _.aspx.cs

--- _.aspx.cs
+++ _.aspx.cs
@@ -1,57 +1,91 @@
-        void loadServices(Excel.Worksheet ws)
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Web.UI;
+using System.Web.Script.Serialization;
+using System.Web.UI.WebControls;
+
+namespace winwin
+{
+    public partial class _default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
         {
-            Excel.Range range = ws.UsedRange;
-            for (int i = 2; i <= range.Rows.Count; i++)
+            string folderPath = @"C:\Temp\Winwin\AdminRemote";
+
+            try
             {
-                HtmlGenericControl div = new HtmlGenericControl("div");
-                div.Attributes.Add("class", "service-panel");
-                HtmlGenericControl h3 = new HtmlGenericControl("h3");
-                h3.ID = range.Rows[i].Cells[3].Text;
-                h3.InnerText = range.Rows[i].Cells[1].Text;
-                HtmlGenericControl p = new HtmlGenericControl("p");
-                p.InnerText = range.Rows[i].Cells[2].Text;
-                div.Controls.Add(h3);
-                div.Controls.Add(p);
-                services.Controls.Add(div);
+                var csvFiles = Directory.GetFiles(folderPath, "*.csv");
+
+                if (csvFiles.Length == 0)
+                {
+                    Response.Write("CSV dosyası bulunamadı.");
+                    return;
+                }
+
+                string latestFile = csvFiles.OrderByDescending(f => new FileInfo(f).CreationTime).First();
+                List<string[]> rows = new List<string[]>();
+
+                using (StreamReader reader = new StreamReader(latestFile))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (!string.IsNullOrEmpty(line))
+                        {
+                            string[] cells = line.Split('+').Select(c => c.Trim()).ToArray();
+                            if (cells[0] == "Admin")
+                            {
+                                string[] cell = { cells[1], cells[3] };
+                                rows.Add(cell);
+                            }
+                        }
+                    }
+                }
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                serializer.MaxJsonLength = Int32.MaxValue;
+                string json = serializer.Serialize(rows);
+                string script = $"<script> data = {json};initializeTable();</script>";
+                ClientScript.RegisterStartupScript(this.GetType(), "initializeData", script);
             }
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(ws);
+            catch (Exception ex)
+            {
+                Response.Write("Hata: " + ex.Message);
+            }
         }
+        protected void hiddenButton_Click(object sender, EventArgs e)
+        {
+            string jsonData = hiddenField.Value;
 
+            if (!string.IsNullOrEmpty(jsonData))
+            {
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                js.MaxJsonLength = int.MaxValue;
+                var tableData = js.Deserialize<List<string[]>>(jsonData);
 
+                if (tableData.Count == 0)
+                {
+                    Response.Write("No data available.");
+                    return;
+                }
 
+                StringBuilder csv = new StringBuilder();
+                csv.AppendLine("Host;User");
 
-                <div class="serv-group">
-                    <div class="serv-group-title">Backend Projects</div>
-                    <div class="serv-item">
-                        <h2 class="serv-title">
-                            API Server <span class="serv-meta">(Public)</span>
-                        </h2>
-                        <p class="serv-desc">gres sofkijmsoa dojkf nsdjkfn </p>
-                    </div>
-                    <div class="serv-item">
-                        <h2 class="serv-title">
-                        Database Manager <span class="serv-meta">(Public)</span>
-                        </h2>
-                        <p class="serv-desc">s dsfsd f,,df efewnia d</p>
-                    </div>
-                </div>
+                foreach (var row in tableData)
+                {
+                    csv.AppendLine($"{row[0]};{row[1]}");
 
-Servis Adı;	Servis Açıklaması;	Servis Linki(IIS Site Adı);	Kategori;	Meta;
-Fiziksel Sunucu Envanteri	Fiziksel Envanter görüntüle ve düzenle...	fiziksel	Inventory	
-Fiziksel Sunucu Envanteri(Historical)	Envanteri tarihe göre incele...	fizikselHistorical	Historic	
-Fiziksel Sunucu Envanteri(Otomasyon)	Fiziksel Envanter görüntüle	hwpedia	Inventory	
-Fiziksel Sunucu Envanteri(Otomasyon-Historical)	Fiziksel otomasyon envanterini tarihe göre incele...	hwpediaHistorical	Historic	Public
-VMWare Envanter	VMWare envanter görüntüle...	vmpedia	Inventory	
-RvTools Backups	RvTools Envanter Yedeklemeleri Görüntüle...	rvtools	Backups	
-Script Control DashBoard	Otomasyondaki Scriptleri Kontrol Et...	ScriptControl	Admin	
-ODM Sanal Sunucu Envanteri	Pendik replikasyonlu TEST, PRD sunucuları görüntüle	odmvms	Inventory	Public
-Created and Deleted VMs	VMWare silinen ve oluşturulan VMlerı görüntüle..	createdanddeletedvms	Inventory	
-Pendik Hall Lists	Pendik Hall Ayrımlı VMleri görüntüle..	hallLists	Inventory	
-CloudUnited Servis Yonetim Paneli	Servisleri için Erişimleri Yönet..	authconfiger	Admin	
+                }
 
-
-
-
-
-
-                            
+                Response.Clear();
+                Response.ContentType = "text/csv";
+                Response.AddHeader("content-disposition", "attachment;filename=administrators.csv");
+                Response.Write(csv.ToString());
+                Response.End();
+            }
+        }
+    }
+}
