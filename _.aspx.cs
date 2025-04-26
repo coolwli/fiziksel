28cf30b9df2eb4e61f62f455f2c7e9a4ea7f52eb
ali candar <118173006+coolwli@users.noreply.github.com>
2025-04-26 17:14:25 +0300

    Update _.aspx.cs

--- _.aspx.cs
+++ _.aspx.cs
@@ -1,9 +1,1 @@
-{
-    "name": "APGARTSTVCS201",
-    "vcenter": "APGARTSTVCS201",
-    "location": "Pendik",
-    "cpu_core": "1184",
-    "intel_count": "22",
-    "amd_count": "0",
-    "tarih": "4/25/2025 12:00:00 AM"
-}
+
